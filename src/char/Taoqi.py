00d9b970c60c8bf914c41b3f71229c4d7b0e0ee5
firedcto@gmail.com <firedcto@gmail.com>
2024-06-12 01:21:16 +0800

    1

--- src/char/Taoqi.py
+++ src/char/Taoqi.py
@@ -2,4 +2,21 @@ from src.char.BaseChar import BaseChar
 
 
 class Taoqi(BaseChar):
-    pass
+    def perform(self):
+        if self.has_intro:
+            self.normal_attack()
+            self.sleep(0.3)
+            self.normal_attack()
+            self.sleep(0.3)
+            self.normal_attack()
+            self.sleep(0.3)
+        if self.liberation_available():
+            self.click_liberation()
+            self.sleep(2)
+        if self.resonance_available():
+            self.click_resonance()
+            if self.echo_available():
+                self.sleep(0.3)
+                self.click_echo()
+            self.sleep(0.3)
+        self.switch_next_char()
--- src/char/Verina.py
+++ src/char/Verina.py
@@ -2,4 +2,23 @@ from src.char.BaseChar import BaseChar
 
 
 class Verina(BaseChar):
-    pass
+
+    def perform(self):
+        if self.liberation_available():
+            self.click_liberation()
+            self.sleep(1.5)
+        if self.resonance_available():
+            self.click_resonance()
+            if self.echo_available():
+                self.sleep(0.3)
+                self.click_echo()
+            self.sleep(0.3)
+        else:
+            self.heavy_attack()
+        self.switch_next_char()
+
+    def do_get_switch_priority(self, current_char, has_intro=False):
+        if has_intro:
+            return -3
+        else:
+            return 2
