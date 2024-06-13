8839961780c2334429b6f1814b04bde274cf9c45
firedcto@gmail.com <firedcto@gmail.com>
2024-06-14 02:13:13 +0800

    add hovac rover

--- src/char/CharFactory.py
+++ src/char/CharFactory.py
@@ -2,16 +2,19 @@ def get_char_by_pos(task, box, index):
     from src.char.Verina import Verina
     from src.char.Yinlin import Yinlin
     from src.char.Taoqi import Taoqi
+    from src.char.BaseChar import BaseChar
+    from src.char.HavocRover import HavocRover
     char_dict = {
-        'char_yinlin': Yinlin,
-        'char_verina': Verina,
-        'char_taoqi': Taoqi
+        'char_yinlin': {'cls': Yinlin, 'res_cd': 12, 'echo_cd': 15},
+        'char_verina': {'cls': Verina, 'res_cd': 12, 'echo_cd': 20},
+        'char_taoqi': {'cls': Taoqi, 'res_cd': 15, 'echo_cd': 20},
+        'char_rover': {'cls': HavocRover, 'res_cd': 12, 'echo_cd': 20}
     }
 
-    for char_name, char in char_dict.items():
+    for char_name, char_info in char_dict.items():
         feature = task.find_feature(char_name, box=box, threshold=0.9)
         if feature:
             task.log_info(f'found char {char_name}')
-            return char(task, index)
+            return char_info.get('cls')(task, index, char_info.get('res_cd'))
     task.log_info(f'could not find char')
-    return None
+    return BaseChar(task, index)
