f8ec4e55473ad9f407ba03ee61477723e368269b
firedcto@gmail.com <firedcto@gmail.com>
2024-06-15 19:29:31 +0800

    add compressed featureset

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7909638aab438a93fb1d99f0fb4ee4198846d0d98db92ceff7cbdefcdecdf005
-size 639
+oid sha256:b9789c47a09ad806a2d5e9e3a5b5be3056278af97977415c9a289ed2861e13c8
+size 540
--- src/task/SkipDialogTask.py
+++ src/task/SkipDialogTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ce53fbb5a87ec7212ddd42d51ee3f705a2b9915e6603471f235765a6a034055
-size 1824
+oid sha256:5c916cb0f6eaa8691aacfd6139a42815bf0788ea386ec554b14c0e42fa3c377e
+size 3052
