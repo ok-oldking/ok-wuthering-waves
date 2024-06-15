b52cbf220122241376b4d2718df19ffe315b0a54
firedcto@gmail.com <firedcto@gmail.com>
2024-06-15 19:29:31 +0800

    add compressed featureset

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -3,20 +3,18 @@ from src.char.BaseChar import BaseChar
 
 class Sanhua(BaseChar):
     def do_perform(self):
-        if self.has_intro:
-            self.sleep(0.8)
-            self.normal_attack()
-            self.sleep(.4)
-            self.normal_attack()
-            self.sleep(.4)
-            self.normal_attack()
         if self.liberation_available():
             self.click_liberation()
             self.sleep(2)
         if self.resonance_available():
             self.click_resonance()
-            if self.echo_available():
-                self.sleep(0.3)
-                self.click_echo()
             self.sleep(0.3)
+            if self.echo_available():
+                self.click_echo()
+                self.sleep(0.3)
+        else:
+            self.task.mouse_down()
+            self.sleep(1)
+            self.task.mouse_up()
+
         self.switch_next_char()
