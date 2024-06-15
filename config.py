1023b6d68b83beb54cfe79881e81645cb4af4fcf
firedcto@gmail.com <firedcto@gmail.com>
2024-06-15 08:26:46 +0800

    tuning autocombat

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ config = {
     #     'use_proxy': True
     # },
     'about': """
-    <h3>OK白荆</h3>
+    <h3>OK-WW</h3>
     <p>免费开源软件 <a href="https://github.com/ok-oldking/ok-baijing">https://github.com/ok-oldking/ok-baijing</></p>
     <p>报告问题BUG <a href="https://github.com/ok-oldking/ok-baijing/issues/new?assignees=ok-oldking&labels=bug&projects=&template=%E6%8A%A5%E5%91%8Abug-.md&title=%5BBUG%5D">https://github.com/ok-oldking/ok-baijing/issues/new?assignees=ok-oldking&labels=bug&projects=&template=%E6%8A%A5%E5%91%8Abug-.md&title=%5BBUG%5D</></p>
     <p>视频演示 <a href="https://www.bilibili.com/video/BV1K7421f7KT/">https://www.bilibili.com/video/BV1K7421f7KT/</a></p>
@@ -48,7 +48,7 @@ config = {
 """,
     'supported_screen_ratio': '16:9',
     'screenshots_folder': "screenshots",
-    'gui_title': 'OK白荆',  # Optional
+    'gui_title': 'OK-WW',  # Optional
     # 'coco_feature_folder': get_path(__file__, 'assets/coco_feature'),  # required if using feature detection
     'log_file': 'logs/ok-script.log',  # Optional, auto rotating every day
     'error_log_file': 'logs/ok-script_error.log',
