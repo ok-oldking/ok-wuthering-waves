90a918219e55eff4d6324f00a5abce5665712be1
firedcto@gmail.com <firedcto@gmail.com>
2024-06-21 09:56:50 +0800

    try click resonance/echo util cd is deteced

--- src/char/Verina.py
+++ src/char/Verina.py
@@ -6,13 +6,9 @@ class Verina(BaseChar):
     def do_perform(self):
         if self.flying():
             return self.switch_next_char()
-        if self.liberation_available():
-            self.click_liberation()
-        if self.resonance_available():
-            self.click_resonance()
-            if self.echo_available():
-                self.sleep(0.3)
-                self.click_echo()
+        self.click_liberation()
+        self.click_resonance()
+        self.click_echo()
         self.heavy_attack()
         self.switch_next_char()
 
