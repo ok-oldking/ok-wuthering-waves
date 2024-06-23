37ab1a8f05c7044ffaa0c36e6428c1a80518e10a
firedcto@gmail.com <firedcto@gmail.com>
2024-06-23 12:18:30 +0800

    try first build

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ config = {
     'debug': False,  # Optional, default: False
     'use_gui': True,
     'config_folder': 'configs',
-    'gui_icon': get_path_in_package(__file__, 'icon.ico'),
+    'gui_icon': get_path_in_package(__file__, 'icon.png'),
     'ocr': {
         'lib': 'RapidOCR',
         'inference_num_threads': int(os.cpu_count() / 2)
@@ -39,19 +39,18 @@ config = {
     'update': {
         'releases_url': 'https://api.github.com/repos/ok-oldking/ok-wuthering-waves/releases?per_page=15',
         'proxy_url': 'https://gh.ok-script.com/',
-        'exe_name': 'ok-baijing.exe',
+        'exe_name': 'ok-ww.exe',
         'use_proxy': True
     },
     'about': """
-    <h3>OK-WW</h3>
-    <p>免费开源软件 <a href="https://github.com/ok-oldking/ok-baijing">https://github.com/ok-oldking/ok-baijing</></p>
-    <p>报告问题BUG <a href="https://github.com/ok-oldking/ok-baijing/issues/new?assignees=ok-oldking&labels=bug&projects=&template=%E6%8A%A5%E5%91%8Abug-.md&title=%5BBUG%5D">https://github.com/ok-oldking/ok-baijing/issues/new?assignees=ok-oldking&labels=bug&projects=&template=%E6%8A%A5%E5%91%8Abug-.md&title=%5BBUG%5D</></p>
-    <p>视频演示 <a href="https://www.bilibili.com/video/BV1K7421f7KT/">https://www.bilibili.com/video/BV1K7421f7KT/</a></p>
+    <h3>OK-Wuthering-Waves</h3>
+    <p>免费开源软件 <a href="https://github.com/ok-oldking/ok-wuthering-waves">https://github.com/ok-oldking/ok-wuthering-waves</></p>
+    <p>报告问题BUG <a href="https://github.com/ok-oldking/ok-wuthering-waves/issues/new?assignees=ok-oldking&labels=bug&projects=&template=%E6%8A%A5%E5%91%8Abug-.md&title=%5BBUG%5D">https://github.com/ok-oldking/ok-baijing/issues/new?assignees=ok-oldking&labels=bug&projects=&template=%E6%8A%A5%E5%91%8Abug-.md&title=%5BBUG%5D</></p>
     <p>QQ群:<a href="https://qm.qq.com/q/aGO7eBJ2Uw">594495691</a></p>
 """,
     'supported_screen_ratio': '16:9',
     'screenshots_folder': "screenshots",
-    'gui_title': 'OK-WW',  # Optional
+    'gui_title': 'OK-Wuthering-Waves',  # Optional
     # 'coco_feature_folder': get_path(__file__, 'assets/coco_feature'),  # required if using feature detection
     'log_file': 'logs/ok-script.log',  # Optional, auto rotating every day
     'error_log_file': 'logs/ok-script_error.log',
