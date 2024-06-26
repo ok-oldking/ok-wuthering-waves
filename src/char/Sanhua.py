129399a1a95e35d7ca0a5f372221015b6f65f926
firedcto@gmail.com <firedcto@gmail.com>
2024-06-27 01:18:24 +0800

    adjust characters

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -8,17 +8,15 @@ class Sanhua(BaseChar):
         need_switch = False
         if self.resonance_available():
             self.click_resonance()
-            self.sleep(0.2)
             need_switch = True
         if self.echo_available():
             self.click_echo()
-            self.sleep(0.3)
             need_switch = True
 
         if not need_switch:
             self.task.mouse_down()
             self.sleep(.7)
             self.task.mouse_up()
-            self.sleep(0.2)
+            self.sleep(0.3)
 
         self.switch_next_char()
