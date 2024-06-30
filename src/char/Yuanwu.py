33fadbb9cd01be5e33e6e3ce31a81f480de68dec
firedcto@gmail.com <firedcto@gmail.com>
2024-06-30 20:56:30 +0800

    optimize jinhsi

--- src/char/Yuanwu.py
+++ src/char/Yuanwu.py
@@ -2,20 +2,6 @@ from src.char.BaseChar import BaseChar
 
 
 class Yuanwu(BaseChar):
-    def do_perform(self):
-        self.logger.debug(
-            f'Encore_perform_{self.has_intro}_{self.echo_available()}_{self.resonance_available()}_{self.liberation_available()}')
-        if self.click_liberation():
 
-        elif self.resonance_available():
-            self.click_resonance()
-        elif self.echo_available():
-            self.click_echo(duration=1.5)
-        elif self.is_forte_full():
-            self.heavy_attack()
-        else:
-            self.logger.info('Encore nothing is available')
-        self.switch_next_char()
-
-    def n4(self):
-        self.continues_normal_attack(2.5)
+    def count_resonance_priority(self):
+        return 0
