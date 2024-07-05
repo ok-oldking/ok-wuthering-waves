55e8a758c66d3db7c4626a35ebf6002d0464cf76
firedcto@gmail.com <firedcto@gmail.com>
2024-07-06 01:20:06 +0800

    increase time searching jue

--- src/task/FarmEchoTask.py
+++ src/task/FarmEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e74753bf78f76aecd9259cb36220d18e5eeefbf4478ece4ed03b580021e2ede
-size 6913
+oid sha256:71912c3823885bfae05cf893d93de304405f28f3e00778e71b44e4c6dae74881
+size 7007
