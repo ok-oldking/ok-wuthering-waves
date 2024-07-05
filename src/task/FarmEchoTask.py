4f09b35f86cef0b3ddd33f880490de2632cfe080
firedcto@gmail.com <firedcto@gmail.com>
2024-07-05 14:59:21 +0800

    increase sleep after exit from echo

--- src/task/FarmEchoTask.py
+++ src/task/FarmEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c61c3944846b13e1761c57fdbfa006d5f0761b81059f8a269254d39e53dabbc7
+oid sha256:137d9140aa1b643fcb97bf4e06e85df6495095c65d90779dd67d271ac3d50e5c
 size 6867
