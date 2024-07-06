fcd2f4b902abb4e0e4e56b63ba63b69231feb8a1
firedcto@gmail.com <firedcto@gmail.com>
2024-07-06 20:56:25 +0800

    fix encore

--- src/char/Encore.py
+++ src/char/Encore.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:741fb38a5b50d20e76821c7abbb9af310b7ad7e5f03c6dc2d985a9fc8658272f
-size 1484
+oid sha256:5510a5b6456ddf682f3a8c0ebbc8e12f154848fb87861744d7f3ed270059f85a
+size 1520
