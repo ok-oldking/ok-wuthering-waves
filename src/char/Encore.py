1c171605f480bb9a4eb40be8aa5f629b1ce1b66d
firedcto@gmail.com <firedcto@gmail.com>
2024-07-09 11:16:48 +0800

    optimize encore, fix out of combat when casting liberation

--- src/char/Encore.py
+++ src/char/Encore.py
@@ -8,39 +8,75 @@ class Encore(BaseChar):
     def __init__(self, *args):
         super().__init__(*args)
         self.last_heavy = 0
+        self.liberation_time = 0
+        self.last_resonance = 0
+
+    def still_in_liberation(self):
+        return time.time() - self.liberation_time < 10
 
     def do_perform(self):
-        self.logger.debug(
-            f'Encore_perform_{self.has_intro}_{self.echo_available()}_{self.resonance_available()}_{self.liberation_available()}')
-        if self.click_liberation():
-            self.n4()
-            self.click_resonance()
-            self.n4()
-            self.task.right_click()
-            self.sleep(0.4)
-            self.n4()
-            self.click_resonance()
-            # full = self.is_forte_full()
-            # if full:
-            #     self.logger.info('Encore is_forte_full cast')
-            #     self.sleep(2)
+        target_low_con = False
+        if self.has_intro:
+            self.sleep(0.7)
+        self.wait_down()
+        if self.can_resonance_step2():
+            if self.click_resonance()[0]:
+                self.logger.info('try Encore resonance_step2 success')
+                self.sleep(0.3)
+            else:
+                self.logger.info('try Encore resonance_step2 failure')
+
+        if self.still_in_liberation():
+            # if time.time() - self.liberation_time > 7.5 and self.is_forte_full():
             #     self.heavy_attack()
             #     self.last_heavy = time.time()
-        elif self.resonance_available():
-            self.click_resonance()
+            # else:
+            target_low_con = True
+            self.n4()
+        elif self.click_resonance()[0]:
+            self.logger.debug('click_resonance')
+            self.last_resonance = time.time()
+            pass
+        elif self.click_liberation():
+            self.liberation_time = time.time()
+            self.n4()
+            target_low_con = True
         elif self.echo_available():
+            self.logger.debug('click_echo')
             self.click_echo(duration=1.5)
         else:
             self.logger.info('Encore nothing is available')
-        self.switch_next_char()
+        self.switch_next_char(target_low_con=target_low_con)
+
+    def count_liberation_priority(self):
+        return 40
+
+    def count_resonance_priority(self):
+        return 40
+
+    def count_echo_priority(self):
+        return 40
+
+    def can_resonance_step2(self):
+        return time.time() - self.last_resonance < 4
 
     def do_get_switch_priority(self, current_char: BaseChar, has_intro=False):
-        if time.time() - self.last_heavy < 4:
-            self.logger.info(
-                f'switch priority MIN because heavy attack not finished')
+        if time.time() - self.last_heavy < 3:
             return Priority.MIN
+        elif self.still_in_liberation() or self.can_resonance_step2() or has_intro:
+            self.logger.info(
+                f'switch priority MIN because still in liberation')
+            return Priority.MAX
         else:
             return super().do_get_switch_priority(current_char, has_intro)
 
-    def n4(self):
-        self.continues_normal_attack(2.4)
+    def n4(self, duration=2.0):
+        duration = 2.6 if self.click_resonance()[0] else 2.3
+        if time.time() - self.liberation_time < 6 or not self.is_forte_full():
+            self.continues_normal_attack(duration=duration)
+            if not self.still_in_liberation():
+                self.click_resonance()
+        else:
+            self.heavy_attack()
+            self.logger.info('encore heavy')
+            self.last_heavy = time.time()
