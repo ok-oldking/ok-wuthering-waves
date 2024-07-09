102c25a29e7b534a894e610ef0dd9a45e70b6fdc
firedcto@gmail.com <firedcto@gmail.com>
2024-07-09 11:16:48 +0800

    optimize encore, fix out of combat when casting liberation

--- src/char/BaseChar.py
+++ src/char/BaseChar.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53f356f18f50d5218b884972c951bd2be6f57ab784602634ec2e0598682c709d
-size 23162
+oid sha256:20599f6450b19ac28ab8bccfdc30928ec2082acf229a4eb25af40ed0232bda48
+size 23226
