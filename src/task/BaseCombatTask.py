0a46d71396cccaeeff2db5c20f575f8cb78a5a8c
firedcto@gmail.com <firedcto@gmail.com>
2024-07-10 10:30:02 +0800

    close claim reward dialog

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab458b6a260aee80df5484bac3ba5dadfb6e9066ff31244985470e24493c34d7
-size 12662
+oid sha256:0d0b26580af9dca47f403e89b83f05861a2a82ac0570454eeb73915df2780c8a
+size 12760
