902a496c0ec6809a9690e667930d8cd1c07e7468
firedcto@gmail.com <firedcto@gmail.com>
2024-07-10 11:44:39 +0800

    fix 2560x1440 scrolling

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8c70720123c033f169b3d96259efbef5d9c857560d1650e63e3bd62f1b5d2f1
+oid sha256:a8e22223a2cd9aa3e44a0ac20631716a247c500843a54a73fb410160dd5924f0
 size 7508
