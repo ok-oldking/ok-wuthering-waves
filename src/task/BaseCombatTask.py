2cd658110da2ed3e32e1d3b7dc04fa5b94ca5aff
firedcto@gmail.com <firedcto@gmail.com>
2024-07-12 17:59:11 +0800

    click f faster

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f59d8c3729aaa1d983b9d83861c4f2738fc2cf6abfd7f1cc0a8612cc7e53345
-size 14062
+oid sha256:a33daa6a8f22b6089a60f834edf0d95a325d14a245ce47463479faea0a530bc3
+size 14114
