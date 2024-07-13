2a115b70c4c3a303e41f891d6103dcc5a2e0eabd
firedcto@gmail.com <firedcto@gmail.com>
2024-07-13 18:34:54 +0800

    fix boss scrolling, change to click to scroll

--- src/char/Encore.py
+++ src/char/Encore.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b48cfded798cf4dce760c574565c1f9589fa99068ef48e31598ae4cf14062fc4
-size 2918
+oid sha256:263fbcce489f20cbaad20641461d31d4e7f710ebe96f5b5d2ef87f113ab527c9
+size 2922
--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9544361c3d7fe23a252df0ba6bb3a08453c88ef479f92046b67acebc27bda3fb
-size 7665
+oid sha256:3dd9dd022c1b4c3fcd6af398a765dc91ff538869864ee3eed1e5ed2d0dd7a8c9
+size 7872
