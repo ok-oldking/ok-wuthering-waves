47ac82727f342bc0f25cbb8937e7c76abeb322e9
firedcto@gmail.com <firedcto@gmail.com>
2024-07-14 20:39:11 +0800

    修复传送点到经验书和没完成新手任务无法刷c4

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fada0cba39e4ccd83a64b7d4a8635fa8897c6d24d2af4b6136442a4d8b47a3e0
-size 7920
+oid sha256:657aac7e8c8b982fef0e0776b376694e792fb463ae382138798a73dec72525e0
+size 7856
