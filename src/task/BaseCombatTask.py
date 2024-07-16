4731b5279bbe13977ac085b30e2586252f787c81
firedcto@gmail.com <firedcto@gmail.com>
2024-07-16 16:51:11 +0800

    try catch mouse exception

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63fbd435e15b1171b0218a2a6246ab94f8b66eb2cfa72e8802bfb1fe4afbcb16
-size 14254
+oid sha256:db7655627372d8217b2443587fea091c3234d8cbff353ccb06ab6c31e9128ca3
+size 14415
