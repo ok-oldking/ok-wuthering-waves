d946a3aa9ebb5e46c6b133156d4424eb484de7aa
firedcto@gmail.com <firedcto@gmail.com>
2024-07-17 15:10:11 +0800

    修复无法开启无冠者

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f98776abf718e6e28d0382fdd0ad65d3535a203ec5266c16d27fc45c43fd94e
-size 2771
+oid sha256:9f974c5e12af9492e3c877b06ad97ba09a2b13fb897a2b9e8f3e65a1b34f1971
+size 2952
--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74f060e3df892cd64af32c404e0f1af590ac654be804a57bbe13c648e2653b23
-size 8010
+oid sha256:594290e29e7dbe6d83c137d45f98585b1107b1ca1a8f96d95c38c4cd9c5ffbd4
+size 8184
