07c7fa71cc96374d2b762ceefe4c925aa0fca57c
firedcto@gmail.com <firedcto@gmail.com>
2024-07-21 22:11:00 +0800

    添加自动拾取

--- src/task/AutoCombatTask.py
+++ src/task/AutoCombatTask.py
@@ -9,6 +9,7 @@ class AutoCombatTask(BaseCombatTask, TriggerTask):
 
     def __init__(self):
         super().__init__()
+        self.trigger_interval = 0.2
         self.name = "Auto Combat"
         self.description = "Enable auto combat in Abyss, Game World etc"
 
