152110c6ad6eacb16deeb1431d26b8567416b8bf
firedcto@gmail.com <firedcto@gmail.com>
2024-07-21 22:57:06 +0800

    修复自动拾取

--- src/task/AutoPickTask.py
+++ src/task/AutoPickTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b343fde12341c06b62e4e32610de8dc675aaeec5020a6aaba75003b3c41cee4e
-size 1475
+oid sha256:5e83798af29ca9cb72e2156d4b2adc89b98ce2dd42b34c016f40ca1d1eb793d4
+size 1433
