8ec1faaf969238c850e94dc8f062a142430e71fe
firedcto@gmail.com <firedcto@gmail.com>
2024-07-22 15:57:46 +0800

    修复卡卡罗入场技跳出战斗

--- src/char/Calcharo.py
+++ src/char/Calcharo.py
@@ -1,11 +1,5 @@
-from typing import Any
-
 from src.char.BaseChar import BaseChar
 
 
 class Calcharo(BaseChar):
-
-    def switch_next_char(self, *args: Any, **kwargs: Any):
-        if self.is_con_full():
-            self.task.in_liberation = True
-        super().switch_next_char(*args, **kwargs)
+    pass
