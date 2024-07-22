edd724b51ba98d17a6c33262077ff3bf6ce047ba
firedcto@gmail.com <firedcto@gmail.com>
2024-07-22 16:03:32 +0800

    修复卡卡罗入场技跳出战斗

--- src/char/Calcharo.py
+++ src/char/Calcharo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ab5515fd4d8252df68ecc9668894dbc8e4f50c51ce1d5fdd10b03cecb03c117
-size 76
+oid sha256:ffbce708811d9e02fff6b59cf3448f5587b87c006290ef685299c803406fa580
+size 290
--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58321f134cd82e58831117bbaef0d20a6c664ae2c071236fca8258e597b91683
-size 14720
+oid sha256:771d7c0ede09c50a3706e3db34a3380e807ef38f150804c5232bd04fc8ccdbad
+size 14228
