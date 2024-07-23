e09d053cb321a3b1c9bd4cc0c95611ca2997ca0e
firedcto@gmail.com <firedcto@gmail.com>
2024-07-23 09:28:50 +0800

    修复刷声骸某些情况可能卡领取奖励(fix #16)

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:184e3fb1f5428d607c17b7864d4f4185ef3832e3ec4bf0aaf02c1e17c21f6050
-size 14265
+oid sha256:70f26b5d75ee73e2940b4bdff2e4f10bdd9c097508c62a634b96ab48cfcc0d45
+size 14294
