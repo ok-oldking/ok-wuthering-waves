5eb9cf075b736444d392e49a32a2ae2e4477441c
firedcto@gmail.com <firedcto@gmail.com>
2024-07-24 11:46:50 +0800

    优化声骸拾取

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26108663f0dfb5fafcac5809ea046225a49d3a3154f332ab6227b2d708a24a71
-size 8621
+oid sha256:c7d33e1fd8cf09c5f8447426d8953835377e734b70c3a72b33cf5738ba0e4680
+size 8505
