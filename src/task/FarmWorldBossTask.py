e8dcd5e2866324a089947d4765da9016e681528a
firedcto@gmail.com <firedcto@gmail.com>
2024-07-25 01:26:04 +0800

    修复无冠者可能卡住,  修复可能esc跳出

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -165,6 +165,9 @@ class FarmWorldBossTask(BaseCombatTask):
                             self.sleep(2)
                             logger.info('Crownless walk to f')
                             self.walk_until_f(raise_if_not_found=True, time_out=4, backward_time=1)
+                            in_combat = self.wait_until(self.in_combat, raise_if_not_found=False, time_out=10)
+                            if not in_combat:  # try click again
+                                self.walk_until_f(raise_if_not_found=True, time_out=4)
                         try:
                             self.combat_once()
                         except CharDeadException:
