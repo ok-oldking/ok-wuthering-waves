a8b8596754cf569adca93faca815dfaca0f86a78
firedcto@gmail.com <firedcto@gmail.com>
2024-07-25 22:06:39 +0800

    优化声骸拾取

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:343fdb7cbe2c604f10013c82b6b2e91138fe5a5aece13543bd287597b5b06af6
-size 14744
+oid sha256:7142592cc3cd2959c7063d2ef54941115800cce61009b6f323af3547e13a72b5
+size 14650
