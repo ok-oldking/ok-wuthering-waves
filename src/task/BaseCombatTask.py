d66fd29c7163e960159e031386713d7d4742b064
firedcto@gmail.com <firedcto@gmail.com>
2024-07-26 22:37:02 +0800

    修复没体力卡领取

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c98caafe5a8eb0c37e2285385e2cb21ff494bc1e29245ccdfc56a12de7ec26e
-size 14312
+oid sha256:f337b9e40a1426f05d535a03ede75af71cb6fcc8197614b5c7ec9780e4a7c17a
+size 14311
