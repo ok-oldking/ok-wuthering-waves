bc3e9361a5b6ec9dbeab38eab6161ebd616bb9f2
firedcto@gmail.com <firedcto@gmail.com>
2024-07-26 12:31:43 +0800

    检查小月卡, 需要手动开启配置

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,4 +1,6 @@
 import re
+import time
+from datetime import datetime, timedelta
 
 from ok.config.ConfigOption import ConfigOption
 from ok.feature.FindFeature import FindFeature
@@ -13,12 +15,29 @@ pick_echo_config_option = ConfigOption('Pick Echo Config', {
 }, config_description={
     'Use OCR': 'Turn on if your CPU is Powerful for more accuracy'})
 
+monthly_card_config_option = ConfigOption('Monthly Card Config', {
+    'Check Monthly Card': False,
+    'Monthly Card Time': 4
+}, config_description={
+    'Check Monthly Card': 'Check for monthly card to avoid interruption of tasks',
+    'Monthly Card Time': 'Your computer\'s local time when the monthly card will popup, hour in (1-24)'
+})
+
 
 class BaseWWTask(BaseTask, FindFeature, OCR):
 
     def __init__(self):
         super().__init__()
         self.pick_echo_config = self.get_config(pick_echo_config_option)
+        self.monthly_card_config = self.get_config(monthly_card_config_option)
+        self.next_monthly_card_start = 0
+
+    def validate(self, key, value):
+        message = self.validate_config(key, value)
+        if message:
+            return False, message
+        else:
+            return True, None
 
     @property
     def absorb_echo_text(self):
@@ -37,6 +56,21 @@ class BaseWWTask(BaseTask, FindFeature, OCR):
             return lang_feature
         else:
             return None
+
+    def set_check_monthly_card(self):
+        if self.monthly_card_config.get('Check Monthly Card'):
+            now = datetime.now()
+            hour = self.monthly_card_config.get('Monthly Card Time')
+            # Calculate the next 4 o'clock in the morning
+            next_four_am = now.replace(hour=hour, minute=0, second=0, microsecond=0)
+            if now >= next_four_am:
+                next_four_am += timedelta(days=1)
+            next_monthly_card_start_date_time = next_four_am - timedelta(seconds=30)
+            # Subtract 1 minute from the next 4 o'clock in the morning
+            self.next_monthly_card_start = next_monthly_card_start_date_time.timestamp()
+            logger.info('set next monthly card start time to {}'.format(next_monthly_card_start_date_time))
+        else:
+            self.next_monthly_card_start = 0
 
     @property
     def f_search_box(self):
@@ -58,6 +92,47 @@ class BaseWWTask(BaseTask, FindFeature, OCR):
             if not text:
                 return None
         return f
+
+    def check_for_monthly_card(self):
+        if self.next_monthly_card_start > 0:
+            if time.time() > self.next_monthly_card_start:
+                start = time.time()
+                logger.info(f'start waiting for monthly card')
+                f4_open = False
+                if self.in_team_and_world():
+                    logger.info(f'in team send f4 to wait')
+                    self.send_key('f4')
+                    f4_open = True
+                monthly_card = self.wait_until(self.handle_monthly_card, time_out=120, raise_if_not_found=False)
+                logger.info(f'wait monthly card end {monthly_card}')
+                if f4_open:
+                    self.send_key('esc')
+                    self.sleep(2)
+                    logger.info(f'wait monthly card close f4')
+                cost = time.time() - start
+                self.set_check_monthly_card()
+                return cost
+        return 0
+
+    def sleep(self, timeout):
+        return super().sleep(timeout - self.check_for_monthly_card())
+
+    def wait_in_team_and_world(self, time_out=10, raise_if_not_found=True):
+        return self.wait_until(self.in_team_and_world, time_out=time_out, raise_if_not_found=raise_if_not_found)
+
+    def in_team_and_world(self):
+        return self.in_team()[
+            0]  # and self.find_one(f'gray_book_button', threshold=0.7, canny_lower=50, canny_higher=150)
+
+    def handle_monthly_card(self):
+        monthly_card = self.find_one('monthly_card', threshold=0.8)
+        if monthly_card is not None:
+            self.click(monthly_card)
+            self.sleep(2)
+            self.click(monthly_card)
+            self.sleep(1)
+        logger.debug(f'check_monthly_card {monthly_card}')
+        return monthly_card is not None
 
     @property
     def game_lang(self):
