71571090fe3418106b40af814be452a6939d0648
firedcto@gmail.com <firedcto@gmail.com>
2024-07-26 21:11:35 +0800

    修复可能卡ESC

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49ba602705e2af3b24d9089d5df3643d51e88b3d95d88879b9ed14f48dc17c6d
-size 14277
+oid sha256:8c98caafe5a8eb0c37e2285385e2cb21ff494bc1e29245ccdfc56a12de7ec26e
+size 14312
