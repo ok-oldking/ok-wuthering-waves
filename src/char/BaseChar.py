b3d65d4d754c9fdbcd84175528cb77e743829e42
firedcto@gmail.com <firedcto@gmail.com>
2024-07-27 23:48:33 +0800

    修改大招检查

--- src/char/BaseChar.py
+++ src/char/BaseChar.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9050229815d2064c768e6f74edc2d38193b44ce0c4a83be514f548f59f531a20
-size 24501
+oid sha256:220bcf16cf45f574049fd063ea0546318e6277daf6d053d7bd99de2c9d79bfe1
+size 24394
