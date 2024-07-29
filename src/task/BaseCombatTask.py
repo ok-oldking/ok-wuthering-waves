d5c6dee15dd2115edce0f3f3705220452fd01c4f
firedcto@gmail.com <firedcto@gmail.com>
2024-07-29 14:53:46 +0800

    增加刷boss等待时间

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ebd44e8f58c85864f429547250d2d7473773ad48ce0f17b9f5fa545198c9486
-size 15875
+oid sha256:07978ecee4bb98a9a12fb8eed413e69041758f28f4e1c0df49ff68e7f660d8c6
+size 15873
