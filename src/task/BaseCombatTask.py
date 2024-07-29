3f41fbc5d1e488e79c22c8c80d34ff947509d333
firedcto@gmail.com <firedcto@gmail.com>
2024-07-29 15:38:57 +0800

    修复两人队伍会异常

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07978ecee4bb98a9a12fb8eed413e69041758f28f4e1c0df49ff68e7f660d8c6
-size 15873
+oid sha256:8d7b43ed10ea04b393a84a296bdb642ce76c68accf88a1227018381e688d4da6
+size 15883
