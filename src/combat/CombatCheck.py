1b07ded0c11518773e0dae3c78b683852ca5458c
firedcto@gmail.com <firedcto@gmail.com>
2024-07-30 18:50:33 +0800

    优化自动战斗

--- src/combat/CombatCheck.py
+++ src/combat/CombatCheck.py
@@ -29,7 +29,7 @@ class CombatCheck:
         self.last_liberation = 0
 
     def reset_to_false(self, recheck=False, reason=""):
-        if self.handle_monthly_card():
+        if self.should_check_monthly_card() and self.handle_monthly_card():
             return True
         if is_pure_black(self.frame):
             logger.error('getting a pure black frame for unknown reason, reset_to_false return true')
