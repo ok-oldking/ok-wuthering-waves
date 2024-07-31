4f25dc4225476346a3598fd788049ed15113e5e7
firedcto@gmail.com <firedcto@gmail.com>
2024-07-31 09:02:56 +0800

    尝试修复月卡问题 优化今汐渊武

--- src/char/Yuanwu.py
+++ src/char/Yuanwu.py
@@ -16,5 +16,8 @@ class Yuanwu(BaseChar):
         self.click_liberation(con_less_than=1)
         if self.is_forte_full():
             self.send_resonance_key(down_time=0.6, post_sleep=0.2)
-        self.click_echo()
+        elif self.click_echo():
+            pass
+        else:
+            self.continues_normal_attack(0.2)
         self.switch_next_char()
