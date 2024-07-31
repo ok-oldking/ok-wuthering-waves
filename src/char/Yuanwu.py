9c9b5e2addca889ef2570bf3b6f4c1211d2fc1f4
firedcto@gmail.com <firedcto@gmail.com>
2024-08-01 00:17:47 +0800

    修复某些机器今汐强化E不喷

--- src/char/Yuanwu.py
+++ src/char/Yuanwu.py
@@ -14,10 +14,12 @@ class Yuanwu(BaseChar):
 
     def do_perform(self):
         self.click_liberation(con_less_than=1)
+        if self.has_intro:
+            self.continues_normal_attack(1.2)
         if self.is_forte_full():
-            self.send_resonance_key(down_time=0.6, post_sleep=0.2)
+            self.send_resonance_key(down_time=0.7, post_sleep=0.2)
         elif self.click_echo():
             pass
         else:
-            self.continues_normal_attack(0.2)
+            self.continues_normal_attack(0.31)
         self.switch_next_char()
