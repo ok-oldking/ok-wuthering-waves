20e8529a29e65f07ce771cca33455260b4ac90ae
firedcto@gmail.com <firedcto@gmail.com>
2024-08-02 23:10:47 +0800

    跳出战斗不截图

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8734ebc7ae01c5d49e33e0de5e241ead809d16f5ba9c7ec5e6c69985cdcbe049
-size 13533
+oid sha256:7bdba85dd39ba7384ee2d7967db36cb7d21be963168054287da4a7cbea4c9292
+size 13535
