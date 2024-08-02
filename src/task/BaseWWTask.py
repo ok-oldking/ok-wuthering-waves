98feb359490b72cb3507464eb772af7265ee425f
firedcto@gmail.com <firedcto@gmail.com>
2024-08-02 10:34:26 +0800

    优化搜索声骸

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -7,6 +7,7 @@ from ok.feature.FindFeature import FindFeature
 from ok.logging.Logger import get_logger
 from ok.ocr.OCR import OCR
 from ok.task.BaseTask import BaseTask
+from ok.task.TaskExecutor import CannotFindException
 
 logger = get_logger(__name__)
 
@@ -108,6 +109,110 @@ class BaseWWTask(BaseTask, FindFeature, OCR):
                 return cost
         return 0
 
+    def walk_find_echo(self, backward_time=1):
+        if self.walk_until_f(time_out=6, backward_time=backward_time, target_text=self.absorb_echo_text,
+                             raise_if_not_found=False):  # find and pick echo
+            logger.debug(f'farm echo found echo move forward walk_until_f to find echo')
+            self.incr_drop(True)
+
+    def walk_until_f(self, direction='w', time_out=0, raise_if_not_found=True, backward_time=0, target_text=None):
+        logger.info(f'walk_until_f direction {direction} target_text: {target_text}')
+        if not self.find_f_with_text(target_text=target_text):
+            if backward_time > 0:
+                if self.send_key_and_wait_f('s', raise_if_not_found, backward_time, target_text=target_text):
+                    logger.info('walk backward found f')
+                    return True
+            return self.send_key_and_wait_f(direction, raise_if_not_found, time_out,
+                                            target_text=target_text) and self.sleep(0.5)
+        else:
+            self.send_key('f')
+            if self.handle_claim_button():
+                return False
+        self.sleep(0.5)
+        return True
+
+    def send_key_and_wait_f(self, direction, raise_if_not_found, time_out, running=False, target_text=None):
+        if time_out <= 0:
+            return
+        start = time.time()
+        if running:
+            self.mouse_down(key='right')
+        self.send_key_down(direction)
+        f_found = self.wait_until(lambda: self.find_f_with_text(target_text=target_text), time_out=time_out,
+                                  raise_if_not_found=False)
+        if f_found:
+            self.send_key('f')
+            self.sleep(0.1)
+        self.send_key_up(direction)
+        if running:
+            self.mouse_up(key='right')
+        if not f_found:
+            if raise_if_not_found:
+                raise CannotFindException('cant find the f to enter')
+            else:
+                logger.warning(f"can't find the f to enter")
+                return False
+
+        remaining = time.time() - start
+
+        if self.handle_claim_button():
+            self.sleep(0.5)
+            self.send_key_down(direction)
+            if running:
+                self.mouse_down(key='right')
+            self.sleep(remaining + 0.2)
+            if running:
+                self.mouse_up(key='right')
+            self.send_key_up(direction)
+            return False
+        return f_found
+
+    def handle_claim_button(self):
+        if self.wait_feature('claim_cancel_button_hcenter_vcenter', raise_if_not_found=False, horizontal_variance=0.05,
+                             vertical_variance=0.1, time_out=1.5, threshold=0.8):
+            self.sleep(0.5)
+            self.send_key('esc')
+            self.sleep(0.5)
+            logger.info(f"found a claim reward")
+            return True
+
+    def turn_and_find_echo(self):
+        box = self.box_of_screen(0.25, 0.20, 0.75, 0.53, hcenter=True)
+        highest_percent = 0
+        highest_index = 0
+        threshold = 0.01
+        for i in range(4):
+            self.middle_click_relative(0.5, 0.5)
+            self.sleep(1)
+            color_percent = self.calculate_color_percentage(echo_color, box)
+            if color_percent > highest_percent:
+                highest_percent = color_percent
+                highest_index = i
+                if color_percent > threshold:
+                    self.log_debug(f'found color_percent {color_percent} > {threshold}, walk now')
+                    return self.walk_find_echo(backward_time=0)
+            if self.debug:
+                self.screenshot(f'find_echo_{highest_index}_{float(color_percent):.3f}_{float(highest_percent):.3f}')
+            logger.debug(f'searching for echo {i} {float(color_percent):.3f} {float(highest_percent):.3f}')
+            # self.click_relative(0.25, 0.25)
+            self.send_key('a', down_time=0.05)
+            self.sleep(0.5)
+
+        if highest_percent > 0.0001:
+            for i in range((highest_index + 1) % 4):
+                self.middle_click_relative(0.5, 0.5)
+                self.sleep(0.5)
+                self.send_key('a', down_time=0.05)
+                self.sleep(0.5)
+            if self.debug:
+                self.screenshot(f'pick_echo_{highest_index}')
+            logger.info(f'found echo {highest_index} walk')
+            return self.walk_find_echo(backward_time=0)
+
+    def incr_drop(self, dropped):
+        if dropped:
+            self.info['Echo Count'] = self.info.get('Echo Count', 0) + 1
+
     def should_check_monthly_card(self):
         if self.next_monthly_card_start > 0:
             if 0 < time.time() - self.next_monthly_card_start < 120:
@@ -148,3 +253,10 @@ class BaseWWTask(BaseTask, FindFeature, OCR):
         elif 'Wuthering' in self.hwnd_title:
             return 'en_US'
         return 'unknown_lang'
+
+
+echo_color = {
+    'r': (200, 255),  # Red range
+    'g': (150, 220),  # Green range
+    'b': (130, 170)  # Blue range
+}
