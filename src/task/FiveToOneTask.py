13dfd382693f40694b337b4a7ba5f9231abfa78e
firedcto@gmail.com <firedcto@gmail.com>
2024-08-05 00:35:48 +0800

    五合一ocr优化

--- src/task/FiveToOneTask.py
+++ src/task/FiveToOneTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1274d93b849769bc084d11758c8a7e6f71ab92a77af640d70bfdf219afa2f320
-size 10192
+oid sha256:e9d3811a42e2cfecad6aacdf13a88dd120e40201eebe5f1217c601ce435d32c7
+size 10305
