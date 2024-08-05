1bb9354c6c421ec46509e0d7c840f29146a09d57
firedcto@gmail.com <firedcto@gmail.com>
2024-08-05 16:57:09 +0800

    五合一优化找不到套装的问题

--- src/task/FiveToOneTask.py
+++ src/task/FiveToOneTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84ab8790af28edd8a5e6cdb0b7e8ac7741570764d1cb430a691a7b5faf02a8f3
-size 10382
+oid sha256:0d30c440b06caf1e5cd7b4c25c87f8ee7dc4d499ea890321b2b6f2369979cb8c
+size 11591
