df997ada5c538228fd93f2a8096481186c902d6a
firedcto@gmail.com <firedcto@gmail.com>
2024-08-06 21:24:38 +0800

    修复合成3C卡住

--- src/task/FiveToOneTask.py
+++ src/task/FiveToOneTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:939436cc6ab4878b120b510e7cf1bfab828b22a2a2eaf8b9108cf4a99f26c0c3
-size 13154
+oid sha256:d82c7f29e46fa0412904119f141c3ea69d12d3861a267ff6d68248aeec27a7c1
+size 13155
