f0a3b18400d25a7a1ae75ac07e1f605222d48fc3
i12cu84 <49804511+i12cu84@users.noreply.github.com>
2024-08-09 10:36:34 +0800

    2024.8.9

--- src/char/Verina.py
+++ src/char/Verina.py
@@ -6,13 +6,13 @@ class Verina(BaseChar):
     def do_perform(self):
         self.click_liberation()
         if self.flying():
+            self.normal_attack()
             return self.switch_next_char()
         if self.click_resonance(send_click=False)[0]:
             return self.switch_next_char()
         self.click_echo()
         if self.is_forte_full():
             self.heavy_attack()
-        # self.normal_attack()
         self.switch_next_char()
 
     def count_base_priority(self):
