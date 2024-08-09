431a4e1de5388d16f2b58cb8ba4d073afe686e1b
firedcto@gmail.com <firedcto@gmail.com>
2024-08-09 15:32:49 +0800

    维里奈改成不打重击 修复可能一直卡在战斗状态

--- src/char/Verina.py
+++ src/char/Verina.py
@@ -6,12 +6,13 @@ class Verina(BaseChar):
     def do_perform(self):
         self.click_liberation()
         if self.flying():
+            self.normal_attack()
             return self.switch_next_char()
         if self.click_resonance(send_click=False)[0]:
             return self.switch_next_char()
         self.click_echo()
-        if self.is_forte_full():
-            self.heavy_attack()
+        # if self.is_forte_full():
+        #     self.heavy_attack()
         # self.normal_attack()
         self.switch_next_char()
 
