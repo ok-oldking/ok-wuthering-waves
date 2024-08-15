e54e853a1c79ac59b57d58a13f7c5895a7a72e41
firedcto@gmail.com <firedcto@gmail.com>
2024-08-15 15:59:30 +0800

    适配1.2版本

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e2c443e0d3b7673fd81d230bd28b4f8d2a413816d7fe27a42854604c4b07320
-size 3750
+oid sha256:b0c9e74bfef4e4bf3042c61921f50b3b3b28b118d515ab64b5f5ee631f5bbde1
+size 3839
