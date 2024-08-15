8cd97ea6ee6f4b3e78ee15c8d6d26974cdd8690c
firedcto@gmail.com <firedcto@gmail.com>
2024-08-15 16:55:30 +0800

    五合一适配1.2

--- src/task/FiveToOneTask.py
+++ src/task/FiveToOneTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cd4730f0f026364902f45241667c78516779a25b9b0f1bda2190bf9d6b55eaf
-size 12338
+oid sha256:5d8aac30592f3845cac7b472cdf13ce95e2c2f05115c577d9937e8fcf815e1e4
+size 13292
