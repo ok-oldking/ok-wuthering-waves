7a81c6cc4498bd4aa18a490b5a87442bf885c784
Eymeria <Eymeria@fangtang.com>
2024-08-16 13:14:21 +0800

    给散华增加重击爆冰操作

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -3,11 +3,20 @@ from src.char.BaseChar import BaseChar
 
 class Sanhua(BaseChar):
     def do_perform(self):
+        self.task.mouse_down()
+        self.sleep(0.8)
+        self.task.mouse_up()
+        self.sleep(2.0)
         self.click_liberation()
         if self.click_resonance()[0]:
-            return self.switch_next_char()
+           self.task.mouse_down()
+           self.sleep(1.0)
+           self.task.mouse_up()
+           self.sleep(2.0)
+           self.click_echo()
+           return self.switch_next_char()
         if self.click_echo():
-            return self.switch_next_char()
+           return self.switch_next_char()
 
         self.task.mouse_down()
         self.sleep(.7)
