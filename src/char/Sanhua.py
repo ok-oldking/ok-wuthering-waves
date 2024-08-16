b134a85c738b7fd75712e1824d8bef47f3922b38
Eymeria <Eymeria@fangtang.com>
2024-08-16 15:46:15 +0800

    将声骸技能和重击分开，防止相互打断，去掉开头重击。

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -3,17 +3,12 @@ from src.char.BaseChar import BaseChar
 
 class Sanhua(BaseChar):
     def do_perform(self):
-        self.task.mouse_down()
-        self.sleep(0.8)
-        self.task.mouse_up()
-        self.sleep(2.0)
         self.click_liberation()
         if self.click_resonance()[0]:
            self.task.mouse_down()
-           self.sleep(1.0)
+           self.sleep(0.7)
            self.task.mouse_up()
-           self.sleep(2.0)
-           self.click_echo()
+           self.sleep(0.3)
            return self.switch_next_char()
         if self.click_echo():
            return self.switch_next_char()
