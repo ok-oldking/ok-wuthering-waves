2147c4f3e0559422a8678eab3ff07fb989e3c986
firedcto@gmail.com <firedcto@gmail.com>
2024-08-17 00:26:58 +0800

    fix auto pick

--- src/task/AutoPickTask.py
+++ src/task/AutoPickTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09b244b2894fb023b4bc3d1ac497ab5d21d5fefcdffa40a778094d4d5ea64c78
+oid sha256:45048ab9cae13011151cf5e388a85a15c01b15638b0079464f4b9f2d955acbff
 size 2355
