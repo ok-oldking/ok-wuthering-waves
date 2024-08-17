be9c7fe7950c8315179d94bdb2437361270cede0
Small_Ku <small_ku@outlook.com>
2024-08-18 01:52:47 +0800

    refactor: use walk_until_f

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6c462e6292ffeff1d1d87110f0ea8df925362fc77fed15503c0d6ef99f61b69
-size 11904
+oid sha256:e27021a5f13f315c1fe493f29bd4d76f4ab4e24dddfce6385cdf2ee330c8c153
+size 11721
