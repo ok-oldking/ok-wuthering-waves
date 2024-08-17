ebb88263cb4af3ea0a6d208768258a50b41c725a
Small_Ku <small_ku@outlook.com>
2024-08-17 23:13:51 +0800

    feat: allow fast echo pickup while searching

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7d894a624a3f96528c25a7eb8c27598e985c83dd63b040f3bd4743d952aa2f6
-size 11598
+oid sha256:d6c462e6292ffeff1d1d87110f0ea8df925362fc77fed15503c0d6ef99f61b69
+size 11904
