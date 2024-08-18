75aca6009f368f0297810a20e70de9f8ad637f33
firedcto@gmail.com <firedcto@gmail.com>
2024-08-19 00:06:46 +0800

    优化今武门 修复今汐有时候不喷

--- src/char/Baizhi.py
+++ src/char/Baizhi.py
@@ -4,4 +4,17 @@ from src.char.BaseChar import BaseChar
 class Baizhi(BaseChar):
 
     def count_base_priority(self):
-        return - 1
+        return -1
+
+    def do_perform(self):
+        if self.has_intro:
+            self.logger.debug('has_intro wait click 1.2 sec')
+            self.continues_normal_attack(1.2, click_resonance_if_ready_and_return=True)
+        self.click_liberation(con_less_than=1)
+        self.click_resonance()
+        self.click_echo()
+        if not self.is_con_full():
+            self.logger.debug('continues_normal_attack')
+            self.continues_normal_attack(1.1 - self.time_elapsed_accounting_for_freeze(self.last_perform),
+                                         until_con_full=True)
+        self.switch_next_char()
--- src/char/Yuanwu.py
+++ src/char/Yuanwu.py
@@ -10,7 +10,7 @@ class Yuanwu(BaseChar):
         return 0
 
     def count_base_priority(self):
-        return -1
+        return -2
 
     def do_perform(self):
         self.click_liberation(con_less_than=1)
@@ -19,4 +19,6 @@ class Yuanwu(BaseChar):
         if self.is_forte_full():
             self.send_resonance_key(down_time=0.7, post_sleep=0.2)
         self.click_echo()
+        self.continues_normal_attack(1.1 - self.time_elapsed_accounting_for_freeze(self.last_perform),
+                                     until_con_full=True)
         self.switch_next_char()
