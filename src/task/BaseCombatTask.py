aa0b8c5670b82e0b3ab5934f0e878383a8300d70
firedcto@gmail.com <firedcto@gmail.com>
2024-08-19 10:40:03 +0800

    修复自动战斗bug

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ab79bfad405bc5317c447bdeec12ecf77680500116bd641491a6267b7585f8c
-size 16144
+oid sha256:ba0138868b30cbd3fb79302bfbbd12779d204aa5ece332fff528160a522a4b6a
+size 16020
