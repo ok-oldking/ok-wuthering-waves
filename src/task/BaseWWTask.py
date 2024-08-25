088f1b62456a5d0e55fefa109ac8d7b270f29600
firedcto@gmail.com <firedcto@gmail.com>
2024-08-25 18:31:16 +0800

    修复拾取

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8400a98fd078ca7448690be89982205a9391c4d6a8e4415b02440d9d1525163
-size 11723
+oid sha256:dd0afbd6263b4defec770f1e9dbe66902b186989f4542287e118e50be38e55db
+size 11837
