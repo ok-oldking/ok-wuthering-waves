5447352629a367dc6d1839b76a950f42a62ff8ec
firedcto@gmail.com <firedcto@gmail.com>
2024-08-25 18:37:39 +0800

    修复今汐跳出战斗

--- src/char/Jinhsi.py
+++ src/char/Jinhsi.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d362200be0f1cfafd9179f218a6bdd32c6e0b7e72e0ef167a080721bd76dea21
-size 5396
+oid sha256:3a3def89bb0cbf39733004df35f2294d579db22bd55a13ca63a0689e9b04b184
+size 5395
