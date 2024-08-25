fffb1fb963cafe324c3194c7c4795155c38639f7
firedcto@gmail.com <firedcto@gmail.com>
2024-08-25 18:45:41 +0800

    减少刷boss等待时间

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba0138868b30cbd3fb79302bfbbd12779d204aa5ece332fff528160a522a4b6a
-size 16020
+oid sha256:92857bb0acbae5efffb366a18c6a2e854a1352f0d1b24834d663efb397910993
+size 15938
--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:437bc488254250398f565300aae9ceb2287f88ae9b845661bea8e7d8e6709fdb
-size 9851
+oid sha256:e5dc644f03c1270119cbb1f351cc4be7cc686070bc940b1d4eb3a3a44ac8e201
+size 9934
