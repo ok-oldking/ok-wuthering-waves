f7dfedfd29a16e603a4c2f5a5052eb9bf9f0a5e8
firedcto@gmail.com <firedcto@gmail.com>
2024-08-25 14:38:30 +0800

    修复刷boss本可能卡esc

--- src/task/FarmEchoTask.py
+++ src/task/FarmEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36fab7cd0a8befb6c0db4e75d9d592394724a5c9559c717c53d819f121fd09f1
-size 4038
+oid sha256:bb302c90e3650768d4bca86de3c575d6e3a834427b4117696ef1576379f61a7f
+size 4065
