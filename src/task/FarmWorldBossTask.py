6990f7f6f893d3459c0ec560deda2322b098c3d2
firedcto@gmail.com <firedcto@gmail.com>
2024-08-25 11:03:14 +0800

    修复0.2.9刷4c问题

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b73e9d1ada8f5ed0ddb726c7ef6e18a09e7a58019453b8ac2319ee93a35fd002
-size 9824
+oid sha256:437bc488254250398f565300aae9ceb2287f88ae9b845661bea8e7d8e6709fdb
+size 9851
