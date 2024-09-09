7a9acddb2beeeaafeb15cf3834c8f0869929c4ec
firedcto@gmail.com <firedcto@gmail.com>
2024-09-09 18:37:44 +0800

    添加英伟达显卡优化版, 支持RTX3000以上

--- launcher.py
+++ launcher.py
@@ -1,6 +1,6 @@
+# sys.path.append(os.path.abspath('D:/projects/ok-baijing/python-lib'))
+
 from config import config
 from ok.gui.launcher.Launcher import Launcher
-from ok.logging.Logger import config_logger
 
-config_logger(config)
 Launcher(config).start()
