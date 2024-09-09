cdc279137fdbe8e2360352508be6673f3343d7d8
firedcto@gmail.com <firedcto@gmail.com>
2024-09-09 18:37:44 +0800

    添加英伟达显卡优化版, 支持RTX3000以上

--- launcher.py
+++ launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d23d1d5b4904596d03c190335352032ae09255a31d9c63a94ffe489e967c97f
-size 164
+oid sha256:4cbac0815291aedc0f217c05552a7784a3cb14f7e22ca4340fe6064353a0b589
+size 171
