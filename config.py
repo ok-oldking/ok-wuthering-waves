8ceecdf6564946c81f946cbee97d21c557ce375d
firedcto@gmail.com <firedcto@gmail.com>
2024-09-10 15:04:54 +0800

    修复下载OCR模型错误

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b355d0c04a74a8be68b0c2ecc837b1eb4b5c2467a1f6ebd42ce127f7573becae
-size 3925
+oid sha256:725190f1551ab51dad3b6cfd0b84a83c56c3b0d54ddcf4d702fca90cea6652b3
+size 3932
