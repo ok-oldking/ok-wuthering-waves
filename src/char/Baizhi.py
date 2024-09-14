7b6de7745ddb2d6cd6fd78ea8867afa594cab054
firedcto@gmail.com <firedcto@gmail.com>
2024-09-14 13:33:52 +0800

    优化乡里要, 今汐

--- src/char/Baizhi.py
+++ src/char/Baizhi.py
@@ -15,10 +15,6 @@ class Baizhi(BaseChar):
             self.continues_normal_attack(1.2, click_resonance_if_ready_and_return=True)
         self.click_liberation(con_less_than=1)
         self.click_resonance()
-        if not self.is_con_full():
-            self.logger.debug('continues_normal_attack')
-            self.continues_normal_attack(1.1 - self.time_elapsed_accounting_for_freeze(self.last_perform),
-                                         until_con_full=True)
         if self.get_current_con() > 0.65:
             self.click_echo()
         self.switch_next_char()
