0d93293a7a82f282cdd00c7c09ac37efd98b64a2
firedcto@gmail.com <firedcto@gmail.com>
2024-09-20 13:34:55 +0800

    添加下载以及常见问题链接

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aab227ed07b6fa95403dcac6fd46ce5c95b618e8acd16cfaae6dfd9d4a678e0
-size 4270
+oid sha256:a082c0f810c3d268b03050015ed2ddfc8c0a7e8a0d268548e5f10f0a3cfc621a
+size 5176
