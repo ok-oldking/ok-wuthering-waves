5b585e04ee61d27dab1096ed4b65ae87edb3ced4
firedcto@gmail.com <firedcto@gmail.com>
2024-09-26 11:18:53 +0800

    优化乡里要手法

--- src/char/Xiangliyao.py
+++ src/char/Xiangliyao.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2849ea67251e73c02befdf87547a1e4be97eab608c6f2441307024fe3823756f
-size 1125
+oid sha256:ab47f1406bb4f6f42b02db990669aa6022f11c7634006667baea9063c25c1c07
+size 991
