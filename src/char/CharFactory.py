0c2ca038a168b5cfe895864fd5895dd7faec685a
firedcto@gmail.com <firedcto@gmail.com>
2024-09-29 11:55:45 +0800

    支持守岸人

--- src/char/CharFactory.py
+++ src/char/CharFactory.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e75f51dd4532eaa7aaa2c9fd91b34371342c82b6ee3ee39d90bcb35ac66e97e
-size 3116
+oid sha256:92acb4683c988abd1ee0f6268615c98bc9983b8312b783a441b2b2ac4ac696b0
+size 3240
---
+++ src/char/ShoreKeeper.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eaf4bd08d0f9eca14d36b0e0c9ef47e0a17caeb0306b320083e83fadfcbef3c3
+size 476
