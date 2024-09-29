463bb2d28d63cd5379308008ae5263e974736e71
firedcto@gmail.com <firedcto@gmail.com>
2024-09-29 20:49:20 +0800

    退出游戏时候 取消静音 unmute when app exit

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:791327d3e7c1b3ea280c662cfe87f0e5243e5bc3a258bac809724ca6a10c3542
-size 17118
+oid sha256:3d9d662d11c66345e3899ef756a5c8eda1d72f3da5ca468e75dade4a0f075dc0
+size 17139
