b7a99817acf100b997e8194201acc37e2e6e1ece
firedcto@gmail.com <firedcto@gmail.com>
2024-09-29 13:45:13 +0800

    支持1.3跳过剧情

--- src/char/ShoreKeeper.py
+++ src/char/ShoreKeeper.py
@@ -5,11 +5,7 @@ class ShoreKeeper(Healer):
 
     def do_perform(self):
         self.click_liberation()
-        if self.flying():
-            self.normal_attack()
-            return self.switch_next_char()
-        if self.click_resonance(send_click=False)[0]:
-            return self.switch_next_char()
+        self.click_resonance(send_click=False)
         self.click_echo()
         if self.is_forte_full():
             self.heavy_attack()
