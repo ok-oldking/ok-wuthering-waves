fcffbad651412e496dd1f8a812dde093868730f2
firedcto@gmail.com <firedcto@gmail.com>
2024-09-29 14:31:21 +0800

    优化跳过剧情

--- src/task/SkipDialogTask.py
+++ src/task/SkipDialogTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6e638607ddc666670818a71ed8faf4d76a5bbdab245838a3f910ba6db6a5732
-size 3517
+oid sha256:419fcedcb9715778ff935b50194575d9851670be5d48e30bdd77bd86709f37f5
+size 3529
