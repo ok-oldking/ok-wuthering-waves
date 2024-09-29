4fdd2c1790fdf31983cb1d753c932a95854746cf
firedcto@gmail.com <firedcto@gmail.com>
2024-09-29 14:31:06 +0800

    优化跳过剧情

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0d82d1b64825577ca96d59f1ffb52e1bce31b2b80135085cca396277466352d
-size 14078
+oid sha256:d62ba4ef1101f7f39c99de37903043af824a1ba1b6a4b4c89b238e9e8c7383d5
+size 14077
