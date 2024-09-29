f200cd2d9e925dfe458da3b4126ef64fd837c403
firedcto@gmail.com <firedcto@gmail.com>
2024-09-29 13:55:42 +0800

    优化跳过剧情

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9fc7de1b0d38eab8603088b8266205316e192d7da8dbeaec1242ebb5464446e
-size 24567
+oid sha256:ee2654e986847f357c314e43e2fdd9d20dadc81514947be140a74f7c41a2cbfa
+size 23669
--- src/task/SkipDialogTask.py
+++ src/task/SkipDialogTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:935e6be07ae1fb79e930fec867060a304b664d07d903f6a3d5dc38fd3f9a0ceb
-size 3456
+oid sha256:f6e638607ddc666670818a71ed8faf4d76a5bbdab245838a3f910ba6db6a5732
+size 3517
