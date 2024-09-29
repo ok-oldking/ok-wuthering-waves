f86525a5f9612af6266f5e29277e79ea4bb010ed
firedcto@gmail.com <firedcto@gmail.com>
2024-09-29 13:45:13 +0800

    支持1.3跳过剧情

--- src/char/ShoreKeeper.py
+++ src/char/ShoreKeeper.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaf4bd08d0f9eca14d36b0e0c9ef47e0a17caeb0306b320083e83fadfcbef3c3
-size 476
+oid sha256:f70e2926216e1596c9e95279c97a07bb70f80cdbb3672fc4588f8b1eecd5cf3c
+size 324
--- src/task/SkipDialogTask.py
+++ src/task/SkipDialogTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb234266d88b1e59fe7c922cbf16031e82f96cf32442ec771577938b023027dd
-size 2520
+oid sha256:935e6be07ae1fb79e930fec867060a304b664d07d903f6a3d5dc38fd3f9a0ceb
+size 3456
