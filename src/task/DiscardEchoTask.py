75cddb0989e1eee4c9efc6d5aff8569eb366d71f
firedcto@gmail.com <firedcto@gmail.com>
2024-10-01 00:31:08 +0800

    优化弃置声骸 滚动

--- src/task/DiscardEchoTask.py
+++ src/task/DiscardEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c01cf7ea2ebfbfa48906903ecb1ecb436747022013543843415e40538ec2d09
+oid sha256:e231905546b24a0a3bcd5ac1232793ae5d8e7ddb8a231f0c28f7f030b89b9d7d
 size 9625
