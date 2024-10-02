53ae9c910f001f20d20c1ed347bb41c0fc299d58
firedcto@gmail.com <firedcto@gmail.com>
2024-10-03 00:25:18 +0800

    优化渊武

--- src/char/Jinhsi.py
+++ src/char/Jinhsi.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e940dec89e28734a8340c7b1d4a5f5e3f1ad57fda91974cbbbbd03abd70fcb6
-size 5320
+oid sha256:e7b223d4ce9a240e9cf8a2aae304b5d7815b8d86c4eb43db38f75449b6d70883
+size 5062
