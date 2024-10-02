b20f4f58fc25dabd8b21c5adf660e3458b0f8bb0
firedcto@gmail.com <firedcto@gmail.com>
2024-10-02 18:42:44 +0800

    优化防止鸣潮后台抢鼠标 修复有时候启动器启动需要30秒

--- src/task/MouseResetTask.py
+++ src/task/MouseResetTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d84d37995803800a7a3df79c990062932a794908a019b23332cb60ada39bdbf
-size 2090
+oid sha256:b777657dcb5780fa766ff30b916b421df43582af75346cf487b86ea6cdafcf83
+size 2216
