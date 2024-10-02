e140b2df1b02a8ed5f43dc5d4a0325517fab3897
firedcto@gmail.com <firedcto@gmail.com>
2024-10-02 11:16:28 +0800

    修复合成声骸

--- src/task/DiscardEchoTask.py
+++ src/task/DiscardEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f78a6d26027c5d727785e3512fd43381d3dd38915658447aa9a6e6f09beb86de
-size 9725
+oid sha256:bc77cc0bdefcc7404ba18da94b6c142ac4277b22d880c1c3a883b0d5f31a8608
+size 9755
