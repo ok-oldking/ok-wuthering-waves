ee0d4b059e725cda973957ed7f45bc826a4af4a5
firedcto@gmail.com <firedcto@gmail.com>
2024-10-03 23:59:42 +0800

    修复刷大世界boss死人无法自动复活

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00e7413f601821c3fc8508393e3c5beb872c5990caa126f36680f819bf83c4a7
-size 5609
+oid sha256:24d2e3cd10e587fa2029d1c0a952d84e76aca316d1b0e22265fce31e9a0af41a
+size 5839
