40009d7b9acd448894c98d55774803c6b5b561de
firedcto@gmail.com <firedcto@gmail.com>
2024-10-05 00:02:04 +0800

    修复切人后可能卡住

--- src/task/AutoCombatTask.py
+++ src/task/AutoCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d5536f6a93f350068903aff754d0e53770c24fd0cfba0754086c348f6d9d691
-size 1376
+oid sha256:c3786448da2ef410a83350f0897ccc0386fda43e78c882d4a85091dfaf8d5285
+size 1402
