4993c1d06329e83598499e5a6148afcf259c4542
firedcto@gmail.com <firedcto@gmail.com>
2024-10-06 19:24:53 +0800

    优化无法自动战斗提示 降低治疗切出来的频率

--- src/char/Healer.py
+++ src/char/Healer.py
@@ -3,14 +3,10 @@ from src.char.BaseChar import BaseChar, Priority
 
 class Healer(BaseChar):
 
-    def count_base_priority(self):
-        return -1
-
-    def count_echo_priority(self):
-        return 0
-
     def do_get_switch_priority(self, current_char, has_intro=False, target_low_con=False):
         if has_intro and not target_low_con:
             return Priority.MIN
+        elif not self.resonance_available() and not self.liberation_available() and not target_low_con:
+            return Priority.CURRENT_CHAR_PLUS
         else:
             return super().do_get_switch_priority(current_char, has_intro, target_low_con)
