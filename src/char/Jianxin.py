fba4b32959e9560b6debe14cad336e6341ae73a8
firedcto@gmail.com <firedcto@gmail.com>
2024-10-15 11:43:39 +0800

    优化鉴心 fix #135

--- src/char/Jianxin.py
+++ src/char/Jianxin.py
@@ -4,18 +4,12 @@ from src.char.BaseChar import BaseChar
 class Jianxin(BaseChar):
     def do_perform(self):
         if self.has_intro:
-            self.sleep(0.8)
-        # if self.liberation_available():
-        #     self.click_liberation()
-        #     self.sleep(2)
+            self.continues_normal_attack(1)
+        self.click_liberation()
         if self.is_forte_full():
-            self.task.mouse_down()
-            self.sleep(5.6)
-            self.task.mouse_up()
+            self.heavy_attack(5.6)
         if self.resonance_available():
             self.click_resonance()
-            if self.echo_available():
-                self.sleep(0.3)
-                self.click_echo()
-            self.sleep(0.3)
+        if self.echo_available():
+            self.click_echo(duration=0.8)
         self.switch_next_char()
