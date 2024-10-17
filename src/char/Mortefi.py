3756d2377267e59fe5f6d34a27d441a6daa017b4
firedcto@gmail.com <firedcto@gmail.com>
2024-10-17 18:56:44 +0800

    修复今汐有时候不喷

--- src/char/Mortefi.py
+++ src/char/Mortefi.py
@@ -3,10 +3,7 @@ from src.char.BaseChar import BaseChar
 
 class Mortefi(BaseChar):
     def do_perform(self):
-        if self.has_intro:
-            self.wait_down()
-        else:
-            self.click_liberation()
-            self.click_resonance()
-            self.click_echo(sleep_time=0.1)
+        self.click_liberation()
+        self.click_resonance()
+        self.click_echo()
         self.switch_next_char()
