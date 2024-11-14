1144a1ae3022d59f2e54e7e8d223f17285e77c90
firedcto@gmail.com <firedcto@gmail.com>
2024-11-14 23:20:11 +0800

    修复合成声骸可能打不开界面

--- src/task/DiscardEchoTask.py
+++ src/task/DiscardEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc77cc0bdefcc7404ba18da94b6c142ac4277b22d880c1c3a883b0d5f31a8608
-size 9755
+oid sha256:17c9f97b4826cb6c107ee41dd01ae260eb2e99bacc37bddf7467d73c946b3195
+size 9868
