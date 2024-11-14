96ee4b560d1a6d49e6ccd2d26a20f1ea6f8aad96
firedcto@gmail.com <firedcto@gmail.com>
2024-11-14 15:29:45 +0800

    优化椿自动战斗

--- src/char/Camellya.py
+++ src/char/Camellya.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:468cfa87db1e763ac139992817765e8abc27440fec18344c358c8e6a168cc4f2
-size 2684
+oid sha256:e8015f6ed2cf1c6048e6e32da5240fffb78c2d9a5351c4addd3b3fcd14b40f1c
+size 2712
