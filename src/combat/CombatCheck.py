642f6b07c99dc036ce1aa061804225288ade09db
firedcto@gmail.com <firedcto@gmail.com>
2024-11-15 12:47:54 +0800

    增加自动战斗判断准确性和流畅度

--- src/combat/CombatCheck.py
+++ src/combat/CombatCheck.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f784298ebb427357092c82b8440c8db105373cec639cfe02cf4d5eed6c21403
-size 15198
+oid sha256:b35b2be371fccea56211d69c785f00d2e7c080d87177f448989c0b5b3859e404
+size 13417
