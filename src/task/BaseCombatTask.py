63df89ae4d24e758c74f8afddb2702415714b7ed
firedcto@gmail.com <firedcto@gmail.com>
2024-11-16 02:34:00 +0800

    优化截图方式

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85216de99689120ae53850f91b6eaf311693050d04d0dfc17a6402c5ea3bb2a5
-size 23194
+oid sha256:0e440a4ffc00c3b516f6126fa667b8a864b385b86b6e06f7d6bc7051f8c22bbc
+size 23202
