32d110fbe7cc628bb86e3f2b4e0f8958893f335e
firedcto@gmail.com <firedcto@gmail.com>
2024-11-18 00:17:05 +0800

    优化启动器更新界面和逻辑

--- src/char/Camellya.py
+++ src/char/Camellya.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cefb3e068c6c2d18a59699f0db76bed5e71c11c41b69cf4adda2d12d6ff7d39b
-size 3216
+oid sha256:e461b0dd9c5290348a736c9702b1e5b5c4d9e338e0fa8e39c32f793d92cb89d0
+size 2481
