7bc92a8de2c5f56a2624bcb7291397680f3ea906
firedcto@gmail.com <firedcto@gmail.com>
2024-11-19 14:09:53 +0800

    修复可能无法升级报错的问题

--- src/task/DiscardEchoTask.py
+++ src/task/DiscardEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17c9f97b4826cb6c107ee41dd01ae260eb2e99bacc37bddf7467d73c946b3195
-size 9868
+oid sha256:bf25b910bad225d7ad13d957234281107b1e730d3186f334d04a48f7122613f9
+size 9864
