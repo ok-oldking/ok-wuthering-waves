b222d85bb92d84d904b19c01a5d0902e65af49b8
firedcto@gmail.com <firedcto@gmail.com>
2024-12-07 00:17:25 +0800

    修复某些情况无法判断进入战斗

--- tests/TestCombatCheck.py
+++ tests/TestCombatCheck.py
@@ -13,10 +13,20 @@ class TestCombatCheck(TaskTestCase):
     config = config
 
     def test_in_combat_check(self):
+        self.task.do_reset_to_false()
         self.set_image('tests/images/in_combat.png')
         in_combat = self.task.in_combat()
         self.assertTrue(in_combat)
+
+    def test_in_combat_check2(self):
+        self.task.do_reset_to_false()
         self.set_image('tests/images/in_combat2.png')
+        in_combat = self.task.in_combat()
+        self.assertTrue(in_combat)
+
+    def test_in_combat_check3(self):
+        self.task.do_reset_to_false()
+        self.set_image('tests/images/in_combat3.png')
         in_combat = self.task.in_combat()
         self.assertTrue(in_combat)
         logger.debug('in_combat_check task done')
