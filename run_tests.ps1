acda0f868f9316586a2243af1ba35b9d084b6953
firedcto@gmail.com <firedcto@gmail.com>
2024-12-18 19:00:53 +0800

    启动失败添加错误提示

---
+++ run_tests.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c79562fd5df16c12f5a6a9162b5c38a3d12071cfbe37fd406d3f74615e9254ef
+size 139
--- tests/TestCombatCheck.py
+++ tests/TestCombatCheck.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f7dc4cf1de0672a18ba69701318fc08a5d5eecfdbb085ad67ba6e1219011b97
-size 990
+oid sha256:1757ecc42ff30606d5de314908ab14153cf637a1c37859acbf4b92f0f86035af
+size 977
