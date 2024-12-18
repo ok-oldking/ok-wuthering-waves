92b6c68c758f7a9b97007539e34fc82bec95fb71
firedcto@gmail.com <firedcto@gmail.com>
2024-12-18 18:54:25 +0800

    启动失败添加错误提示

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from config import config
-from ok import OK
+if __name__ == '__main__':
+    from config import config
+    from ok import OK
 
-config = config
-ok = OK(config)
-ok.start()
+    config = config
+    ok = OK(config)
+    ok.start()
