ef0062f90a1952e6825d57aca55c0c4d8d91724e
firedcto@gmail.com <firedcto@gmail.com>
2024-12-18 18:54:25 +0800

    启动失败添加错误提示

--- main_debug.py
+++ main_debug.py
@@ -1,8 +1,9 @@
-from config import config
-from ok.OK import OK
+if __name__ == '__main__':
+    from config import config
+    from ok.OK import OK
 
-config = config
-config['debug'] = True
-# config['click_screenshots_folder'] = "click_screenshots"  # debug用 点击后截图文件夹]
-ok = OK(config)
-ok.start()
+    config = config
+    config['debug'] = True
+    # config['click_screenshots_folder'] = "click_screenshots"  # debug用 点击后截图文件夹]
+    ok = OK(config)
+    ok.start()
