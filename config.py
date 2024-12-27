22544c5c032a3ab08e06ee37027e0e0a2bce41e9
firedcto@gmail.com <firedcto@gmail.com>
2024-12-27 10:32:53 +0800

    优化更新 去掉启动器

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7130a1b2cd88b83eb16040e23d08f321253d5e662c11b39b339088b0b95700e7
-size 5238
+oid sha256:29485ce2cbd2252e03ffa78b3b3d24188e37d63fcee3ca3a9e37d057bfa0c54b
+size 5155
--- src/char/Jinhsi.py
+++ src/char/Jinhsi.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbb44c4840f0a6615fd4f4c4a22941c0081c7d10365553ebd90b57b00e286788
-size 4951
+oid sha256:cc111a45a19f10ebdc60e9a99de4f34ba8ee866d295a040a9a12ee37d92df3e2
+size 4758
