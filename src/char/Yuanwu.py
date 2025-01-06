1d964ac68b881a7e549dbf1f7f7012f61d685084
firedcto@gmail.com <firedcto@gmail.com>
2025-01-06 10:36:14 +0800

    渊武放大后补E

--- src/char/Yuanwu.py
+++ src/char/Yuanwu.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:319386238552ca0cb38cb63c1ef4ca9d326451b79584e6b880d9fa3e8dfb73f3
-size 556
+oid sha256:8b604f75ea81000a5bb7cd6ff084c2b5594de073f760682ba946cbe7070dcdb8
+size 591
