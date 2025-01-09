ab9278a7323c38c1209cad61061b31bb2b476011
firedcto@gmail.com <firedcto@gmail.com>
2025-01-09 19:41:40 +0800

    去掉自动战斗报错提示

--- src/combat/CombatCheck.py
+++ src/combat/CombatCheck.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5ffa602be8e445687f8f17e04a5b5bd99ad33bed2e75e87a334603ea8357fb3
-size 12062
+oid sha256:742fe77d90dbd847b8a5ebfe07d56d28b621d819cc76594c47c9d2a9e9cbd906
+size 12043
