cee2912972bcaa86d8bbf053737ce75777175aa0
medicinemelancholy <1807904567@qq.com>
2025-01-11 22:56:19 +0800

    Update FarmWorldBossTask.py
    添加farm罗蕾莱
    每两轮在传送后调整时间至次日1点

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13a5a978afd7df76b6dc9fb4c629b74a12a0be6e5b3f4a5f987cf4ceabb059dd
-size 5866
+oid sha256:05bff3f3c310006ab460a0f2b57c94076ef598cc74cdd2b5713055646b825441
+size 6485
