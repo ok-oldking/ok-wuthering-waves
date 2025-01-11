6296780d3c265a0ea4047d81936d9ec7e63de705
firedcto@gmail.com <firedcto@gmail.com>
2025-01-11 16:44:45 +0800

    支持洛可可

--- src/char/Roccia.py
+++ src/char/Roccia.py
@@ -10,9 +10,10 @@ class Roccia(BaseChar):
             self.continues_normal_attack(1.2)
         self.click_liberation()
         start = time.time()
+        has_charge = self.is_forte_full()
         if self.click_resonance()[0]:
-            while self.is_forte_full() and time.time() - start < 4:
-                self.click(interval=0.2)
+            while has_charge and self.is_forte_full() and time.time() - start < 4:
+                self.click(interval=0.1)
             return self.switch_next_char()
         self.click_echo()
         self.switch_next_char()
