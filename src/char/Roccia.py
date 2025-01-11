fe9fcddab816c5e6c5af2d3d07b507093253cad8
firedcto@gmail.com <firedcto@gmail.com>
2025-01-11 16:44:45 +0800

    支持洛可可

--- src/char/Roccia.py
+++ src/char/Roccia.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:573d6f175aa852bfa5e01266561844e0bec3872abcf707e70ee50184d2eb6ad3
-size 557
+oid sha256:c78bb30698d8a89673b1fe3d66484fe297af91cca7c9c9953ea408011f16f445
+size 614
