42196ab7e12d0d1bf58f03d44c45b3364c9bd6ec
firedcto@gmail.com <firedcto@gmail.com>
2025-01-13 00:13:27 +0800

    优化散华, 改成预输入重击 优化安可

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -1,16 +1,27 @@
+import time
+
 from src.char.BaseChar import BaseChar
 
 
 class Sanhua(BaseChar):
     def do_perform(self):
+        sleep_time = 0.65
+        self.sleep(0.02)
+        self.task.mouse_down()
+        self.sleep(0.1)
+        start = time.time()
         if self.has_intro:
-            self.continues_normal_attack(1.2)
-        self.click_liberation()
-        if self.click_resonance()[0]:
-            return self.switch_next_char()
-        if self.click_echo():
-            return self.switch_next_char()
-
-        self.heavy_attack(0.75)
-        self.sleep(0.4)
+            sleep_time -= 0.1
+            self.wait_intro(click=False, time_out=1.1)
+        if self.click_liberation(send_click=False):
+            sleep_time += 0.40
+            pass
+        clicked, duration, _ = self.click_resonance(send_click=False)
+        if not clicked:
+            self.click_echo()
+        sleep_time -= self.time_elapsed_accounting_for_freeze(start)
+        self.logger.debug('Sanhua to_sleep {}'.format(sleep_time))
+        self.sleep(sleep_time)
+        self.task.mouse_up()
+        self.sleep(0.45)
         self.switch_next_char()
