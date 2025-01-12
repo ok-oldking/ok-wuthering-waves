8d13e36b736937f2b5316a3b6971f55397d18369
firedcto@gmail.com <firedcto@gmail.com>
2025-01-13 01:29:10 +0800

    支持2.0新声骸套装合成

--- config.py
+++ config.py
@@ -106,6 +106,7 @@ config = {
     'onetime_tasks': [  # tasks to execute
         ["src.task.FarmEchoTask", "FarmEchoTask"],
         ["src.task.FarmWorldBossTask", "FarmWorldBossTask"],
+        ["src.task.FiveToOneTask", "FiveToOneTask"],
         ["src.task.DiagnosisTask", "DiagnosisTask"],
     ], 'trigger_tasks': [
         ["src.task.AutoCombatTask", "AutoCombatTask"],
