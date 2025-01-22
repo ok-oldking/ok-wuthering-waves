c74d4b8e1dcf15d94da2cdf854196cdf286cef1e
firedcto@gmail.com <firedcto@gmail.com>
2025-01-22 23:17:06 +0800

    去掉只能一个治疗的限制

--- src/char/Healer.py
+++ src/char/Healer.py
@@ -5,7 +5,7 @@ class Healer(BaseChar):
 
     def do_get_switch_priority(self, current_char, has_intro=False, target_low_con=False):
         if has_intro and not target_low_con:
-            return Priority.MIN
+            return Priority.CURRENT_CHAR_PLUS
         elif not self.resonance_available() and not self.liberation_available() and not target_low_con:
             return Priority.CURRENT_CHAR_PLUS
         else:
