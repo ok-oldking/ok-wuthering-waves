8400a33adbfd49fdfb29c211d3f18adfa89aa3e7
firedcto@gmail.com <firedcto@gmail.com>
2025-01-23 12:47:38 +0800

    修复自动战斗未识别延奏

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e91950ff5138be928299b7cf85e7b7aa5b68343e14d6d7caa3c5e6a91faeaa2
-size 23398
+oid sha256:8f75fe0a0483c8841429d77878e4a08561e520daf8f83698b625cf16242babd4
+size 23373
