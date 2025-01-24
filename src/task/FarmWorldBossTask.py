ef834f0879bd961497f211f440b08021773846dc
嘉然今天吃什么 <57771987+ddd-zero@users.noreply.github.com>
2025-01-24 19:42:11 +0800

    Update FarmWorldBossTask.py

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -109,7 +109,7 @@ class FarmWorldBossTask(BaseCombatTask):
                             dropped = self.walk_find_echo()
                         self.incr_drop(dropped)
 
-            if count <= 2:
+            if count <= 1:
                 self.log_error('Must choose at least 3 Boss to Farm', notify=True)
                 return
                 
