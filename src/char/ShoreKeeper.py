dc9f3b3f00c16755197da4143188a86310daa914
firedcto@gmail.com <firedcto@gmail.com>
2025-01-25 08:25:23 +0800

    优化洛可可

--- src/char/ShoreKeeper.py
+++ src/char/ShoreKeeper.py
@@ -6,9 +6,8 @@ class ShoreKeeper(Healer):
     def do_perform(self):
         if self.has_intro:
             self.logger.debug('ShoreKeeper wait intro animation')
-            self.sleep(1)
-            self.task.wait_in_team_and_world(time_out=4, raise_if_not_found=False)
-            self.check_combat()
+            # self.task.wait_in_team_and_world(time_out=4, raise_if_not_found=False)
+            # self.check_combat()
         self.click_liberation()
         self.click_resonance(send_click=False)
         self.click_echo()
