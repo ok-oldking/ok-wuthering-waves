0cb7e79e97467cb49fc03923e93bab165299a928
RU <cherish870424@gmail.com>
2025-02-08 18:52:31 +0800

    优化count_rings

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2476b13d0e970e28f910e7b35cb69adc38e4937a3bcd8faf5e91c1d95224b5c3
-size 22603
+oid sha256:01a0c5435fcf97890c3d4c09e2285dc4013b701101c7ef2d24af5ad5873b7d9e
+size 23367
