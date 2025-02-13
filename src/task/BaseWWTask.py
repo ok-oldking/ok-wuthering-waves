6be0f87ae22a3ee7c3ed6b19e284b26fc2438bc1
kay3132 <kay3132@gmail.com>
2025-02-13 17:28:13 +0800

    Revert "2.1 4C World"
    This reverts commit 9a2509411be0a4911c76d3a02d5951115ae7d8e4.

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13dbe3bee20d0e18b4fc99188cab084ca07ccf7ef1daef34e24b39eafa13ba1c
+oid sha256:fef10655e818bceb371c7bf83203dbdc0a4b9efd25676a263804e8138e505ef6
 size 19816
