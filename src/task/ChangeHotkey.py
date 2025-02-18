9d1c13064431306286874c35253316fa022d585c
Suogo <148179498+Suogo@users.noreply.github.com>
2025-02-18 15:45:12 +0800

    Add files via upload

---
+++ src/task/ChangeHotkey.py
@@ -0,0 +1,61 @@
+# from src.task.ChangeHotkey import ChangeHotkey
+# BaseWWTask.py
+# self.send_key(ChangeHotkey.key_map())
+# self.send_key(ChangeHotkey.key_pick())
+#         if boss_name == ChangeHotkey.key_teleportlive():
+
+# BaseCombatTask.py
+# self.send_key(ChangeHotkey.key_char1() )
+class ChangeHotkey :
+    def key_char1() :
+        return 'z'
+
+    def key_char2() :
+        return  'x'
+
+
+    def key_char3() :
+        return  'c'
+
+
+    def key_map() :
+        return  'T'
+
+    def key_pick() :
+        return  'e'
+
+    def key_teleportlive():
+        return  'Tempest Mephis'
+            # 'Bell-Borne Geochelone': [0, 0, False],
+            # 'Dreamless': [0, 2, True],
+            # 'Jue': [0, 3, True],
+            # 'Hecate': [0, 4, True],
+            # 'Tempest Mephis': [0, 5, False],
+            # 'Inferno Rider': [0, 6, False],
+            # 'Impermanence Heron': [1, 0, False],
+            # 'Lampylumen Myriad': [1, 1, False],
+            # 'Feilian Beringal': [1, 2, False],
+            # 'Mourning Aix': [1, 3, False],
+            # 'Crownless': [1, 4, False],
+            # 'Mech Abomination': [1, 5, False],
+            # 'Thundering Mephis': [1, 6, False],
+            # 'Fallacy of No Return':[2, 0, False],
+            # 'Lorelei': [2, 1, False],
+            # 'Sentry Construct': [2, 2, False],
+            # 'Dragon of Dirge': [2, 3, False],
+            # 'Nightmare: Feilian Beringal': [2, 4, False],
+            # 'Nightmare: Impermanence Heron': [2, 5, False],
+            # 'Nightmare: Thundering Mephis': [2, 6, False],
+            # 'Nightmare: Tempest Mephis': [3, 0, False],
+            # 'Nightmare: Crownless': [3, 1, False],
+            # 'Nightmare: Inferno Rider': [3, 2, False],
+            # 'Nightmare: Mourning Aix': [3, 3, False],
+
+    # 切换1号队员
+
+
+    # 切换2号队员
+
+
+
+    # 切换3号队员
