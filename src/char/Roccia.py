c6b66eb413fa550781c8c4dd527c9b6408e14adc
firedcto@gmail.com <firedcto@gmail.com>
2025-02-20 22:40:56 +0800

    优化洛可可

--- src/char/Roccia.py
+++ src/char/Roccia.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34e5477e3ed2f749d8caf8783360aaa8fc283439b7aa187d47f9c75807163b2b
-size 2967
+oid sha256:d205e1bd2614a5a3483cf15b909827a72871f45c840868615a953ae32df25ca1
+size 2987
