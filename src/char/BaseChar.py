b8065c9581e7e2172428fee932796fb27e11bc58
firedcto@gmail.com <firedcto@gmail.com>
2025-03-02 09:56:12 +0800

    优化角色加载

--- src/char/BaseChar.py
+++ src/char/BaseChar.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f347fb48f189b05300b825e963f9ebb49221029fd6d9367b30ae010c20a6fd73
-size 18960
+oid sha256:982bbd4c6eebcfdb08e24145248f46e9551596be6951a1706bf75c822c206618
+size 18994
--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1d78f87e43d1f3a8caa1594b3c2fe68f9fd06f4c1d3a7668b9eaf793fcaf7e6
-size 23067
+oid sha256:cbb3bafa0da85d376ce6dae35ee0b655bb24eb36d807dfeae28fa4638be86250
+size 22685
