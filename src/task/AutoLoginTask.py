b584d0653f6106122bd4f60cddc9eb46f3490b67
firedcto@gmail.com <firedcto@gmail.com>
2025-03-02 17:12:25 +0800

    添加一键清日常和无音区

--- src/task/AutoLoginTask.py
+++ src/task/AutoLoginTask.py
@@ -15,22 +15,11 @@ class AutoLoginTask(BaseWWTask, TriggerTask):
         self.name = "Auto Login"
         self.description = "Auto Login After Game Starts"
         self.icon = FluentIcon.ACCEPT
-        self._logged_in = False
 
     def trigger(self):
         if self._logged_in:
             pass
         elif self.in_team_and_world():
             self._logged_in = True
-        elif self.find_one('login_account', threshold=0.7):
-            self.wait_until(lambda: self.find_one('login_account', threshold=0.7) is None,
-                            pre_action=lambda: self.click_relative(0.5, 0.9),
-                            wait_until_check_delay=3, time_out=30)
-            self.wait_until(lambda: self.find_one('monthly_card', threshold=0.7) or self.in_team_and_world(),
-                            pre_action=lambda: self.click_relative(0.5, 0.9),
-                            wait_until_check_delay=3, time_out=120)
-            self.wait_until(lambda: self.in_team_and_world(),
-                            post_action=lambda: self.click_relative(0.5, 0.9),
-                            wait_until_check_delay=3, time_out=5)
-            self.log_info('Auto Login Success', notify=True)
-            self._logged_in = True
+        else:
+            self.wait_login()
