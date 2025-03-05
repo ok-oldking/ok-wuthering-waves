88691c177ad573c1d2b962ff12a982f54b993a19
firedcto@gmail.com <firedcto@gmail.com>
2025-03-05 22:08:34 +0800

    一键日常增加等待时间

--- src/task/DailyTask.py
+++ src/task/DailyTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7947fb09f37a85e54c3871d24e812264934c82e1ce51d0827bc6956103e2405
-size 2221
+oid sha256:3de0a015ae4649ae2f85dab1ad082999bb81be1ecdc7c1d2c9b291a55e4dc990
+size 2280
--- src/task/SkipDialogTask.py
+++ src/task/SkipDialogTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba6b25da58c6ddb09b08302aba86b1165217574622aee50e85da113fdff558ff
-size 494
+oid sha256:8ee7c8d31ff8e98b5322f23fdc7eecd9639aa2b43fba2dde66449763038a7e32
+size 496
