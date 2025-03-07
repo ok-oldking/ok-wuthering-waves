085b545d6e9d7125d8d0315f52d3de1e279bec27
firedcto@gmail.com <firedcto@gmail.com>
2025-03-07 21:27:30 +0800

    修复一条龙可能卡住

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f96a5e9c6db871b614522d15c24425687e4bfcf555db6bc2bf5a68903fdeded
-size 26331
+oid sha256:de8e4d1744dc52584efaf4a0847b82cad515d08c7b06621023c197f07aede4fc
+size 26383
--- src/task/TacetTask.py
+++ src/task/TacetTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e2ba1e71c13d4ef79e7d2f76b9f6d5467bf5069cffdecdbc370a534eb823e74
+oid sha256:8cf4563d425bbc880d2551e1e1356a1a875d4370ec014a3680c9947f5d201230
 size 3953
