e1cfb5eb67917316646a1e4c3e5fb84e55be356d
firedcto@gmail.com <firedcto@gmail.com>
2025-03-15 14:16:24 +0800

    修复可能领不到大月卡任务 优化运行速度

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d12a28e46f2e2bc2dc14e46b6e9d6354599be92c4d37cea9282a542ba82ed887
-size 6448
+oid sha256:edc1e505270532977422add2b0c550f470e474cc956ee6b987ebc7f7836e4284
+size 6478
--- src/task/DailyTask.py
+++ src/task/DailyTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:620ff31886fc987a560756c1bbd51ae10b3f2679f4907d75f59353f1d26ca0b1
-size 2484
+oid sha256:58c184a421c117f2a9b50c4e79f5e89de81467d07fc471a1eacdd4ed92081154
+size 2479
