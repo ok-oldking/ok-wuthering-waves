36d68ca5eacaca9604c4a0114c73896cffac4f79
firedcto@gmail.com <firedcto@gmail.com>
2025-03-22 01:37:23 +0800

    添加自动强化声骸功能 优化触发任务调度

--- src/task/AutoEnhanceEchoTask.py
+++ src/task/AutoEnhanceEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b62666121c395647e15886695bf6727ec5fc0bdb684f7ff220de2eac6094efa
-size 2237
+oid sha256:5b08e4d6940bbf8d5217a88b4d69d649d6af0be58165efbb34b0c3c04f7da19d
+size 2302
--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec6bf0402d61aac4a7ac6aa707bf735b3a8df4e7b9818936586f1d924c49175c
-size 25276
+oid sha256:66876d95dd87874b98623fedb7951d255f8f7be08eb3a797e8bf8a55057006e6
+size 25275
