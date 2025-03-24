cd4fa63c6abd1ebe3181c0bf3f108f5205436a96
firedcto@gmail.com <firedcto@gmail.com>
2025-03-24 23:47:29 +0800

    ocr优化

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bacbfbec0083c13f1eaf016ad2821d97c529e971100751ed310242633c45ae24
-size 7258
+oid sha256:5187fea01bd28e3125a84dcaa769a36451549b9aa4e88ee2b9a174946ba166b2
+size 7260
