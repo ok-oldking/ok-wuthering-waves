2ccf5cd98939ceddf7f61744de6a65fe71c29b87
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-03-27 23:50:41 +0800

    Update SkipBaseTask.py

--- src/task/SkipBaseTask.py
+++ src/task/SkipBaseTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0df78b84fd18534af76e0ccffd7cacd57c38c4d867a3002e3798b485d56a363a
-size 3280
+oid sha256:43fd7b466c6133fef2c395bd61c543f13828e303cd18010e60e6d209ddd4489f
+size 3285
