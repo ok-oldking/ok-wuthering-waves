01e6014d79b8fb3fea1e33419bcca1f08b162805
firedcto@gmail.com <firedcto@gmail.com>
2025-03-29 11:50:42 +0800

    修复Win10低版本可能无法启动

--- src/task/AutoPickTask.py
+++ src/task/AutoPickTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61a2b91f69528353111502ac6832c35f5640ecc713bc6c99e61a74af03ac3d0f
-size 2931
+oid sha256:94957650da48bc516c65bea8eb1797b68f30db66b6cb32ca9f9d635ccb7eb263
+size 2961
