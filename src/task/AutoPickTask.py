3d823706746f5e83fffa84965213361682607d12
firedcto@gmail.com <firedcto@gmail.com>
2025-03-29 11:16:43 +0800

    自动拾取适配2.2版本弹出动画

--- src/task/AutoPickTask.py
+++ src/task/AutoPickTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c440900c92501f99b0cf73bebff0b13a43c8fa2fb33723aa333ea7cfab051a1b
-size 2496
+oid sha256:61a2b91f69528353111502ac6832c35f5640ecc713bc6c99e61a74af03ac3d0f
+size 2931
