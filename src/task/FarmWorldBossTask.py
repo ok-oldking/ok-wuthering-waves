3b583498168b5cff8411825224561c019cf1bdbc
firedcto@gmail.com <firedcto@gmail.com>
2025-04-02 11:37:11 +0800

    大世界boss 4c死了自动传送治疗

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33c04b91708922f6e98d486bd6c9425179e86ff87a66d038af67e86ad5c8deb3
-size 6662
+oid sha256:b0e108178af2acabb7f9977d34567c9452f0efbaeeb2abb6d3d334469a0a188d
+size 6371
