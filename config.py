294cfce61fd3a21a38636a5ed6f2046848494516
firedcto@gmail.com <firedcto@gmail.com>
2025-04-04 13:05:32 +0800

    添加CNB更新源

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd7f49fa06d92791e45e1dbc8ced1eb5fa2eb1e2f446e26413eee5c597a2abec
-size 7079
+oid sha256:f4b6382979e5c0f9b8b1f4ca335cce4ef149c559cbf87cd19408ac787c5a5f81
+size 7052
