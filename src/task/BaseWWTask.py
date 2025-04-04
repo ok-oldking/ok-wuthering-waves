bd8b5565ce111a3f2449b7dee3cf8acaaf5fdd16
firedcto@gmail.com <firedcto@gmail.com>
2025-04-04 09:28:32 +0800

    修复获得声骸数可能显示不对

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bf2e1e8d639a2ee0ff751009615a66fdd9a4985f74ddeb53865333bbcf549cc
-size 27670
+oid sha256:1a74d77731d2aca082fd8ac023916f93d1389e7502ff5c6ff2f72effbbd739d6
+size 27681
