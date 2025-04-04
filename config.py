11c3530902e21b845fa15c2e746ae122cb1499de
firedcto@gmail.com <firedcto@gmail.com>
2025-04-04 19:31:07 +0800

    修复自动拾取可能无限对话 优化升级速度

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cca00aa7fbaa282452aa27cd7b2c3e0ec35c283055dea52eaed404cc1138dc0
+oid sha256:bcb0eee33c9b3fa0546bc91287f719ca2fd3d0833390d75628e269129c0500ac
 size 6661
--- src/task/AutoPickTask.py
+++ src/task/AutoPickTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94957650da48bc516c65bea8eb1797b68f30db66b6cb32ca9f9d635ccb7eb263
-size 2961
+oid sha256:05c28f6f96c5633ad69b4621a10e8f2cd0470d36de8fcf79a37b15177f0c91fd
+size 2976
