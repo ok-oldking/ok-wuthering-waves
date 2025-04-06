bec0ed10619a16395a734cb6ce769818766f5e58
firedcto@gmail.com <firedcto@gmail.com>
2025-04-06 17:57:38 +0800

    修复角色重复查找

--- src/char/CharFactory.py
+++ src/char/CharFactory.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35f3651b80f3fe13d37f59f96cf38c17a1383a0b95bd38d5f9f63e1ae8d4c6a6
-size 4348
+oid sha256:d694c5ed907ffdc47443f9ffa23b15fbf32cbfd6068329a2492b147acd2003c0
+size 4353
