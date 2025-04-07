ccd09c847e7eb4035938b542bba9ab9d15ae25d6
firedcto@gmail.com <firedcto@gmail.com>
2025-04-07 10:03:49 +0800

    优化声骸拾取

--- src/task/WWOneTimeTask.py
+++ src/task/WWOneTimeTask.py
@@ -6,3 +6,5 @@ class WWOneTimeTask:
     def run(self):
         mouse_reset_task = self.executor.get_task_by_class(MouseResetTask)
         mouse_reset_task.run()
+        self.executor.interaction.activate()
+        self.sleep(0.5)
