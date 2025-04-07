3ef882b65acb03c5dda8c21a27b39b872bfa776d
firedcto@gmail.com <firedcto@gmail.com>
2025-04-07 10:15:31 +0800

    修复无音区有双倍奖励的时候无法完成一条龙

--- src/task/TacetTask.py
+++ src/task/TacetTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15030f56eb039c026bbfcffb34d8cdab213da2de37f601c84e022a809f5aa8b4
-size 4843
+oid sha256:9e2665562bba955fc0562a8b498a92687a37eed282733cca22c00d4a3d2963a8
+size 4844
