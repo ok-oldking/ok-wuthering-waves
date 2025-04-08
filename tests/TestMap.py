1b1a5ca3c9c835077d19c75ce02402f03a19fb54
firedcto@gmail.com <firedcto@gmail.com>
2025-04-08 11:21:01 +0800

    优化寻路刷大地图

--- tests/TestMap.py
+++ tests/TestMap.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from config import config
 from ok.test.TaskTestCase import TaskTestCase
@@ -16,18 +17,17 @@ class TestTacet(TaskTestCase):
         self.logger.info(f'test_find_treasure_icon {angle, box}')
         self.assertTrue(100 <= angle <= 200)
 
-    # def test_find_stars(self):
-    #     self.set_image('tests/images/stars.png')
-    #     stars = self.task.find_stars()
-    #     self.logger.info(f'find_stars {stars}')
-    #     self.assertEqual(3, len(stars))
-    #
-    #     angle1 = self.task.get_angle_to_star(stars[0])
-    #     angle2 = self.task.get_angle_to_star(stars[1])
-    #     angle3 = self.task.get_angle_to_star(stars[2])
-    #     self.assertTrue(100 <= angle1 <= 180)
-    #     self.assertTrue(0 <= angle2 <= 90)
-    #     self.assertTrue(-45 <= angle3 <= 0)
+    def test_find_path(self):
+        self.set_image('tests/images/path.png')
+        self.task.load_stars()
+
+        self.set_image('tests/images/mini_map.png')
+        self.task.my_box = self.task.box_of_screen(0.45, 0.17, 0.62, 0.54)
+        star, distance, angle = self.task.find_direction_angle(screenshot=True)
+        time.sleep(5)
+        self.logger.info(f'test_find_path {star, distance, angle}')
+        self.assertTrue(1 <= distance <= 20)
+
 
 if __name__ == '__main__':
     unittest.main()
