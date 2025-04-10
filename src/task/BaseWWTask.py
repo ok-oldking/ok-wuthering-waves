8a8d91641af48534281a5d36b627f2cfc109ad01
firedcto@gmail.com <firedcto@gmail.com>
2025-04-10 11:38:48 +0800

    调整debug模式截图

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0484a1252ea2a7a62f4a2949a708c5be3c1858b25e4b5f5ba17d3c5c4c1bb3b
-size 31086
+oid sha256:604ad714e92605a3c4ca5ca9075d16fcbddca66df1e81545fb7f284e7f082ca1
+size 31088
