d4f569b0781ac151dfa44ca51f2bc1c136671287
RU <cherish870424@gmail.com>
2025-04-11 12:13:10 +0800

    优化散华

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -5,28 +5,28 @@ from src.char.BaseChar import BaseChar
 
 class Sanhua(BaseChar):
     def do_perform(self):
-        sleep_time = 0.65
-        self.sleep(0.02)
-        self.task.mouse_down()
-        self.sleep(0.1)
+        sleep_time = 0.8
+        if self.has_intro:
+            self.sleep(0.02)
         start = time.time()
+        self.task.mouse_down()
         if self.has_intro:
-            sleep_time -= 0.1
             self.wait_intro(click=False, time_out=1.1)
+        elif self.click_resonance(send_click=False):
+            self.sleep(0.1, False)
         if self.click_liberation(send_click=False):
-            sleep_time += 0.40
-            pass
-        clicked_resonance, duration, _ = self.click_resonance(send_click=False)
-        clicked_echo = self.click_echo()
+            sleep_time += 0.425
         sleep_time -= self.time_elapsed_accounting_for_freeze(start)
-        self.logger.debug('Sanhua to_sleep {}'.format(sleep_time))
-        self.sleep(sleep_time)
+        self.logger.info('Sanhua to_sleep {}'.format(sleep_time))
+        if sleep_time > 0:
+            self.sleep(sleep_time, False)
         self.task.mouse_up()
-        if clicked_resonance and not clicked_echo:
-            after_sleep = 0.6
-        else:
-            after_sleep = 0.3
+        self.sleep(0.6)
         if self.has_intro:
-            after_sleep += 0.8
-        self.sleep(after_sleep)
+            self.click_resonance(send_click=False)
+            self.sleep(0.3)
+        if self.get_current_con() == 1:
+            self.sleep(0.1)
+            self.click_echo()
+            self.sleep(0.05)
         self.switch_next_char()
