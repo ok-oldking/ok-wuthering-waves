dd5ad8608187885acd148fe9f556d9b6c5aa37e8
RU <cherish870424@gmail.com>
2025-04-11 12:13:10 +0800

    优化散华

--- src/char/Sanhua.py
+++ src/char/Sanhua.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d78129091a73949dbd15d4f64ae98d62ad20f1fe507fbb357bc373c98d2ec9a
-size 1014
+oid sha256:d1f7934fc376aa82edbd3e2a5a1d07009687a19a76a163fd6d1607787af64aa2
+size 1020
