59c4c8018676afb98084688dbbbbb005aa1f5797
firedcto@gmail.com <firedcto@gmail.com>
2025-04-16 13:48:56 +0800

    优化声骸拾取

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c081a7006135db99cd5f75e6d8624ff3658f67d7a8b0a45aa230a4667ba40e7
-size 30799
+oid sha256:f84e6fdfe8d8668eba5d2c7a2f7b9a1e89a21a8b2b78474f8bb77ad2e683e22b
+size 31007
