c6c8425d569649236fe2da338f49ae461ee3c97d
firedcto@gmail.com <firedcto@gmail.com>
2025-04-17 09:32:25 +0800

    优化大世界寻路

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a2e7b18d47083ba19edb3b68989aeec662d10ecb1348784074896bdb1eb82a2
-size 31066
+oid sha256:bc7e1b72656283080680196cbebed299f25f019a1c95b57b8b49f34dd4215995
+size 31249
