8032609ed4a14a1392049c3a8ed09fd0868a02b9
firedcto@gmail.com <firedcto@gmail.com>
2025-04-17 08:59:33 +0800

    减少声骸拾取战斗结束等待 增加声骸识别概率 拾取声骸不再转向

--- tests/TestEcho.py
+++ tests/TestEcho.py
@@ -14,12 +14,14 @@ class TestEcho(TaskTestCase):
 
     def test_find_echo(self):
         self.set_image('tests/images/echo.png')
-        echos = self.task.find_echo()
+        echos = self.task.find_echos()
+        self.task.log_info('Found1 {} echos'.format(len(echos)))
         self.assertEqual(1, len(echos))
         time.sleep(1)
         self.task.screenshot('echo1', show_box=True)
         self.set_image('tests/images/echo2.png')
-        echos = self.task.find_echo()
+        echos = self.task.find_echos()
+        self.task.log_info('Found2 {} echos'.format(len(echos)))
         time.sleep(1)
         self.task.screenshot('echo2', show_box=True)
         self.assertEqual(1, len(echos))
