4587e6a77339769b61dd34416b8db681249c339a
Xinwei <zhaizixuan1@163.com>
2025-04-20 09:46:31 +0800

    修复罗蕾莱在第三位时，白天无法自动调整时间的情况

--- src/task/FarmWorldBossTask.py
+++ src/task/FarmWorldBossTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a53a7a6d82d955eb482bac24ba83bde222e73790525fe068b9e030d37476241
-size 5521
+oid sha256:59747cdea43f0ebcf56992786c4a41458d971b9e355d4169991a7ca3e0f65508
+size 5519
