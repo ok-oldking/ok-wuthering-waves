a8519f9799d9d34df6670f3fa7c511b0d017018d
firedcto@gmail.com <firedcto@gmail.com>
2025-04-29 11:43:01 +0800

    添加赞妮识别

--- src/char/CharFactory.py
+++ src/char/CharFactory.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d694c5ed907ffdc47443f9ffa23b15fbf32cbfd6068329a2492b147acd2003c0
-size 4353
+oid sha256:5d151b43eff4f891505fe20a7cb61660921a13912af86fba974260eb3cb4db0f
+size 4449
---
+++ src/char/Zani.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c7fa0eb9527bcd90574c753c2e917549ce6426192ccab298424a0c9a6ecd2da
+size 72
