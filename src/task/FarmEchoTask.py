27e699cdaa90b435c44361bbf538fb7c28bad3b1
firedcto@gmail.com <firedcto@gmail.com>
2025-04-29 22:05:41 +0800

    点击重新挑战前增加等待

--- src/task/FarmEchoTask.py
+++ src/task/FarmEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e23496787e6173b62b20f6e20d5a3a40943ca2fccf114ead4a041d48fe92dd70
-size 2767
+oid sha256:5332d6400595a184833b4edfe130cfd1049e7262585da8b1ec9f56669e93e10e
+size 2802
