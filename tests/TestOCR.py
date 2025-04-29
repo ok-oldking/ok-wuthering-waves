1b762fbdaabf7388fa6c345f17b029816ea09c50
firedcto@gmail.com <firedcto@gmail.com>
2025-04-29 17:12:12 +0800

    修复测试

--- tests/TestOCR.py
+++ tests/TestOCR.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:547fc343d6a3a7d94b0934e942f9f6991713dddf7469391a08eb39fab1fd482f
-size 639
+oid sha256:e530858d29e50214d6bab32c9ae12dc7c0b7b84f1f2ff93c245573f64b55d741
+size 570
