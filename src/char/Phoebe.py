d903b304ed598b5088549e867df778b0aabd08a4
medicinemelancholy <1807904567@qq.com>
2025-05-01 00:08:16 +0800

    Update Phoebe.py & Zani.py
    Add support for Zani.
    Phoebe will change to assistant mode while teammate has Zani.

--- src/char/Phoebe.py
+++ src/char/Phoebe.py
@@ -7,16 +7,20 @@ class Phoebe(BaseChar):
         super().__init__(*args, **kwargs)
         self.first_liberation = False
         self.perform_intro = 0
+        self.attribute = 0
         
     def reset_state(self):
         super().reset_state()
         self.first_liberation = False
         self.perform_intro = 0
+        self.attribute = 0
     
     def flying(self):
         return self.current_resonance() == 0 or self.current_echo() == 0
 
     def do_perform(self):
+        if self.attribute == 0:
+            self.decide_teammate()
         if self.has_intro:
             self.continues_normal_attack(1.5)
             if not self.in_absolutin():
@@ -46,18 +50,28 @@ class Phoebe(BaseChar):
                 self.check_combat()
                 if time.time() - start > 5:
                     break
-                if time.time() - start > 0.5 and not self.in_absolutin():
+                if time.time() - start > 0.8 and not self.in_absolutin():
                     return
                 self.click()
                 self.task.next_frame()
-            self.heavy_attack()
+            self.perform_heavy_attack()
         else:
-            self.heavy_attack(1.2)
+            self.perform_heavy_attack(1.2)
         self.first_liberation = True
         if self.is_con_full():
             self.sleep(0.3)
                 
+    def perform_heavy_attack(self, duration=0.6):
+# FIXME: ui淡入有小概率导致菲比在辅助模式下错识别进错状态
+# FIXME: UI fading in has a small probability of causing Phoebe to misidentify,
+#        and enter the wrong state in assistant mode.
+        if self.attribute == 2 and (self.litany_ready() or not self.in_absolutin()):
+            self.hold_resonance(duration=duration)
+        else:
+            self.heavy_attack(duration=duration)
+
     def click_resonance_once(self):
+        self._resonance_available = False
         start = time.time()
         while self.resonance_available():
             self.check_combat()
@@ -65,6 +79,14 @@ class Phoebe(BaseChar):
                 break
             self.send_resonance_key()
             self.task.next_frame()
+            
+    def hold_resonance(self, duration=0.6):        
+        self.check_combat()
+        self.logger.debug('hold resonance start')
+        self.task.send_key_down(self.get_resonance_key())
+        self.sleep(duration)
+        self.task.send_key_up(self.get_resonance_key())
+        self.logger.debug('hold resonance end')
     
     def litany_ready(self):
         box = self.task.box_of_screen_scaled(3840, 2160, 3149, 1832, 3225, 1857, name='phoebe_resonance', hcenter=True)
@@ -78,10 +100,10 @@ class Phoebe(BaseChar):
         self.logger.info(f'light_percent {light_percent}')
         if not self.in_absolutin():
             self.logger.info(f'litany_light_percent {light_percent}')
-        if light_percent > 0.15 or self.is_forte_full() or self.litany_ready():
+        if light_percent > 0.15 and (self.is_forte_full() or self.litany_ready()):
             return True
         blue_percent = self.task.calculate_color_percentage(phoebe_blue_color, box)
-        return blue_percent > 0.15
+        return blue_percent > 0.15 and self.is_forte_full()
         
     def in_absolutin(self):           
         box = self.task.box_of_screen_scaled(3840, 2160, 1633, 1987, 1671, 2008, name='phoebe_forte', hcenter=True)
@@ -104,6 +126,17 @@ class Phoebe(BaseChar):
             return Priority.MIN
         else:
             return super().do_get_switch_priority(current_char, has_intro)
+            
+    def decide_teammate(self):
+        for i, char in enumerate(self.task.chars):
+            self.logger.debug(f'phoebe teammate char: {char.char_name}')
+            if char.char_name == 'char_zani':
+                self.logger.debug(f'phoebe set attribute: support')
+                self.attribute = 2
+                return
+        self.logger.debug(f'phoebe set attribute: attacker')
+        self.attribute = 1
+        return 
     
             
 phoebe_blue_color = {
