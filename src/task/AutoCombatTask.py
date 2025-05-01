17bc63bfa3249a91d7b97f7e2cb5d3fd79c66aa4
firedcto@gmail.com <firedcto@gmail.com>
2025-05-01 10:42:10 +0800

    删除自动战斗后拾取声骸功能

--- src/task/AutoCombatTask.py
+++ src/task/AutoCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f77f4ae3ace76f4ed196312848df6a218fdfde8d778bb8dfa587d9f1cdb57ed3
-size 1734
+oid sha256:96c51e62d3930f6793609d13992f32ab35cf61939174c49d4d18f4a5dc283f0c
+size 1494
