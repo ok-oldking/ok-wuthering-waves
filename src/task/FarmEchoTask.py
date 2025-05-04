8bbe805227b3222b50ca3d8017404590bc8dcae8
firedcto@gmail.com <firedcto@gmail.com>
2025-05-04 17:26:36 +0800

    修复刷需要重新挑战的boss可能会卡住

--- src/task/FarmEchoTask.py
+++ src/task/FarmEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a3ffa3b7f8cca7bfe2226d29a352e7d15848e73efe9675b133a4938d3091004
-size 3841
+oid sha256:a24040b711e206a634c1c8aca0e97cd295ecc95cd61b0e3e5d82a70e1d4db922
+size 4130
