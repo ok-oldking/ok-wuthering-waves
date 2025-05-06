5d783c80e0097e6d58b6c6e67f8614997a305a04
medicinemelancholy <1807904567@qq.com>
2025-05-06 22:45:20 +0800

    优化赞尼菲比行为
    1.菲比新增检查量表中间星形，同时放宽对蓝色的检测范围，以降低队友为赞尼时错进入光菲的情况
    2.减少菲比的log打印
    3.调整赞尼的二段大释放时机
    4.调整两人声骸的释放时机
    5.略微缩短赞尼大招内平a时的站场时间防肘

--- src/char/Phoebe.py
+++ src/char/Phoebe.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be764fdc8e1f26f3d29ec905eeb445c5cf801f72ddfb3a1c5ca63a6d642bd6f6
-size 6408
+oid sha256:1ce3c0cb0beda2282e9248cef4d703cfda01372b9ea28775c1ffe70a2123b6ca
+size 7786
--- src/char/Zani.py
+++ src/char/Zani.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bffa05cca9510776cc2a71cb0ba1a9f99c4bc5acb4c8f6cb994d3d462ce380f3
-size 6106
+oid sha256:a6ab459540f28e987cd6f1cb9f763dcf5c47ead7bfd7f3171795d342a6674830
+size 5940
