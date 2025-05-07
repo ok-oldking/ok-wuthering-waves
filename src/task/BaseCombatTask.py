dc29793a60f9afeb60c2677d9642e74dadd40fe7
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-05-08 01:04:52 +0800

    Update BaseCombatTask.py

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e02d550fbe53c7b5e6adc98e7ececaacc783ab80ad9bc221d24624e594a725aa
-size 22585
+oid sha256:a3e02f220aedca808f5beb3e2d2e085c31c3d98ea7ade110a7caf8d068ca82f6
+size 22573
