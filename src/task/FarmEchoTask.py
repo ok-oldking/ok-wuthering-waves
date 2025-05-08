b65a14be627b2c40b1a2aa40b126aaa9e5b0d759
firedcto@gmail.com <firedcto@gmail.com>
2025-05-08 09:05:43 +0800

    修复刷副本容易不捡声骸 fix #500

--- src/task/FarmEchoTask.py
+++ src/task/FarmEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:230ebcc68c47077c8feacd08e266656b1771f02b98d84be8d776e300793a6d92
-size 4219
+oid sha256:4b99aff7045da9a0137f1c8f128fbb95872a4eececff3e6aae5d054c9eee6107
+size 4321
