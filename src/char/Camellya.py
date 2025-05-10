c2d8b21db5ae238f6c32d5d8befd8fe91878b406
RU <cherish870424@gmail.com>
2025-05-11 00:25:15 +0800

    优化椿 get_forte使用新算法

--- src/char/Camellya.py
+++ src/char/Camellya.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35cf2e589f980e49d85149b47cd2047a7e078e34bc17ec2537609529b8286530
-size 7922
+oid sha256:07d11dea657346750cccc3f85fe40b1e8f4a539583dfd63b151f8b7845f4e0cc
+size 10845
