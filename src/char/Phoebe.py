ca61338e017931e426353aa80b6c4a53f2cf061f
medicinemelancholy <1807904567@qq.com>
2025-05-15 20:42:15 +0800

    优化菲比赞尼
    菲比：
    1.优化识别
    2.优化按键逻辑
    赞尼
    1.优化r2释放逻辑

--- src/char/Phoebe.py
+++ src/char/Phoebe.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d560bd57b9ac694e9ebe108819d4573c91fbf21fc1c1bab7233a85f4f4337b0
-size 8550
+oid sha256:e9ab4f8ad588235270ebcb9ee87e67c164382ff372d7b0e9b00cb6b3f04f7383
+size 9936
--- src/char/Zani.py
+++ src/char/Zani.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f29cf76049faf75430eaf8c26759f6bfd716b2e4f22b1b3496599105478bfb33
-size 5667
+oid sha256:1fe43adb6dae700f935aacf6390a531ca1089db7dd5e35fd36ee00a6ffbac0d1
+size 5541
