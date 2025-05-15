ce48179328d4064e8516288089c757d357b58039
medicinemelancholy <1807904567@qq.com>
2025-05-15 20:48:23 +0800

    优化珂莱塔
    1.r期间脱战不会继续攻击
    2.释放声骸期间切走时无法切回，因此rq后3秒降低切人权重防止队友发呆

--- src/char/Carlotta.py
+++ src/char/Carlotta.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a78f7a7c36b4b456fb1aa72ff1784053d00ef7eed422831b686ba5f52a93b945
-size 1081
+oid sha256:0d543ce557eeafe79affc0d14521083a07453bb60534cb0efedfdc3eab042be5
+size 4205
