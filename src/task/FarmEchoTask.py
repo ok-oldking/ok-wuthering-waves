3d3209c1054368f4e5c7fb777bd2a121c162b509
firedcto@gmail.com <firedcto@gmail.com>
2025-05-20 18:14:32 +0800

    减少刷4C战斗中拾取声骸后等待

--- src/task/FarmEchoTask.py
+++ src/task/FarmEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab182e629458e3c50b57dd2da4b294f0c207f715f31ac3d1327bf6dd0125116f
-size 4336
+oid sha256:63b18323b58329de457c35e60b7f4a50a1e10332e7484f25815fd55a1ddb42ee
+size 4342
