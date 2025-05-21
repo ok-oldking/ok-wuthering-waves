9b02b967dad65465c0d4b7503902c2effc89cf57
firedcto@gmail.com <firedcto@gmail.com>
2025-05-21 22:08:05 +0800

    优化协奏条判断

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a57a752839fb73c09dd32d102547c042c73ebf246cc8d2b036448e49be3eb1a7
-size 24159
+oid sha256:5eadce5187cac7b2993bbdb6b5f8b980ddd98c9f7cb81fb3f97790f4244848fd
+size 23563
