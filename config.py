b24cf522a0e12cdc21192645f6a76dca923723f5
firedcto@gmail.com <firedcto@gmail.com>
2025-05-21 11:18:33 +0800

    尝试修复无法更新

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d8f2858622328468719a63b79f14b9af1c75cf5efe840ee432931219c195cc2
-size 7043
+oid sha256:db341a4a2f2fe00121a25e3c18cee3f3ed3fb3f5e39cb4fb737f4a0e98227411
+size 7133
