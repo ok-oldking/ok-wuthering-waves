608dad44aead9fde40466ba27feac2def68f25d4
firedcto@gmail.com <firedcto@gmail.com>
2025-05-23 00:38:18 +0800

    修复周常肉鸽自动战斗错误

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -331,7 +331,7 @@ class BaseWWTask(BaseTask):
         return illusive_realm_exit is not None
 
     def in_illusive_realm(self):
-        return self.in_realm() and self.find_one('illusive_realm_menu', threshold=0.6)
+        return self.find_one('new_realm_4') and self.in_realm() and self.find_one('illusive_realm_menu', threshold=0.6)
 
     def walk_until_f(self, direction='w', time_out=0, raise_if_not_found=True, backward_time=0, target_text=None,
                      cancel=True):
