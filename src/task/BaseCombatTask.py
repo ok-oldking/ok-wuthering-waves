c8a3f86934b1f0f15bf466705eea850b83fb801e
RU <cherish870424@gmail.com>
2025-05-23 14:15:54 +0800

    修正is_full_ring误判的可能性

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc426ab99d0bc6fe4342bcae0cf57ecf1a298e11e2430a52024314e9446c08d7
-size 23167
+oid sha256:c7eff6c940308bd25dce2c94a659c5251ecad86a857e3dc06277346ca2b598b4
+size 23302
