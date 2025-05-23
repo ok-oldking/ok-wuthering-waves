4e2205107b8bd88d234833814d2d082c425670fe
medicinemelancholy <1807904567@qq.com>
2025-05-23 11:40:27 +0800

    优化忌炎夏空
    忌炎
    修bug
    夏空
    登场结束演奏状态后不再锁切人

--- src/char/Jiyan.py
+++ src/char/Jiyan.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f87d44be8d64ae4cf16b2969d826cf388c813c65906f2aebcf82c075d71ac76
-size 1187
+oid sha256:9896ce484e93a87abab488c6098124965a73f04a1288d678debe9eee42f162ca
+size 1224
