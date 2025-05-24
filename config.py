dbb8ef15c445ce8536bca4cc8371640e772b90a9
firedcto@gmail.com <firedcto@gmail.com>
2025-05-24 18:26:09 +0800

    添加设置, 默认将非16:9分辨率自动调整为16:9

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db341a4a2f2fe00121a25e3c18cee3f3ed3fb3f5e39cb4fb737f4a0e98227411
-size 7133
+oid sha256:df44ecf074a043faa9f3fc93cba669fd23f949d6d000ac17a8a26e6364de5176
+size 7211
