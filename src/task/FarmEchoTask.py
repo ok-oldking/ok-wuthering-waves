3e0f2792612fe50c1afa75ebaeb31b84d78bad16
firedcto@gmail.com <firedcto@gmail.com>
2025-05-24 13:44:00 +0800

    刷4C增加点击重新挑战等待

--- src/task/FarmEchoTask.py
+++ src/task/FarmEchoTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75e02ccd4c69abaabf9491650587677209adc3210727db2aa8564b3b9e23f461
-size 5063
+oid sha256:fcf23e048c948d06898d948ffa63292a941e1fb84249e607b205966bc55eaf98
+size 5087
