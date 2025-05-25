85da71c67ece3503e9c9408656219acb06c7ab42
firedcto@gmail.com <firedcto@gmail.com>
2025-05-25 14:08:35 +0800

    优化洛可可, 丹瑾

--- src/char/BaseChar.py
+++ src/char/BaseChar.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0ca6bc7c245cd05b586a0a9065c3fef66d764bf681995d22ae93224be607e06
+oid sha256:dc13173383cb108bee9179570efb1384ed57be908931f7bc1ec9b69e9a998cd9
 size 30287
