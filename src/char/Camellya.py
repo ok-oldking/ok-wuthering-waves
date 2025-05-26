1324ae1ae9ac5fbf5b2d05e5849e284caf674d3d
RU <cherish870424@gmail.com>
2025-05-26 18:33:44 +0800

    优化椿 放宽颜色范围

--- src/char/Camellya.py
+++ src/char/Camellya.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ac3ce0ef6565c8e00c0e7a445c5505a5b3810ed092489f61a2596255ceba45f
+oid sha256:15e89190b80470f97ce2f7e863761b0c8859e4a5dd006bffae5fc44c7cb4916c
 size 14146
