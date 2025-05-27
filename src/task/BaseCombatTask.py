edf58e84d66136b4a9758c7ed439a25437fb6cb9
RU <cherish870424@gmail.com>
2025-05-28 02:46:24 +0800

    修复切人可能获取到错误的协奏能量

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5248353a63394ab54e2fb152ad3ab01d94e170390e6ac8ae9975b0e598ae2b61
-size 31697
+oid sha256:aca7aa84052a058d56223d5346df8d3f343e39a8cc612fa9ae684204e3b63a80
+size 31764
