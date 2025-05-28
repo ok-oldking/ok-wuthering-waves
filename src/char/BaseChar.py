053d7283e0ba2252bb823600d238227a1a65a0bc
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-05-28 13:13:27 +0800

    优化洛可可

--- src/char/BaseChar.py
+++ src/char/BaseChar.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9506fd4fd0505b8f9ea36d8cf312852723aad6ad4b6f12cb7db6c5e17bdc6ead
-size 40878
+oid sha256:a89cb41c34ccdb9de98eed36323c05fedfbf83a4a663a6dba741a46aa9544eed
+size 41133
--- src/char/Roccia.py
+++ src/char/Roccia.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:031476bf086612edb570d2b9e8007be9fdb70d0147fb9ed439937e51dd9aaea4
-size 3501
+oid sha256:a4d016c138da3fa0426638570a59316a06f968b044159454a86e1ff6f3daf91d
+size 3607
