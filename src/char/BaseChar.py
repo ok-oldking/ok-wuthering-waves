06c9d741989a5d8837aed10633a5d377e27d8fd5
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-05-28 13:13:27 +0800

    优化洛可可

--- src/char/BaseChar.py
+++ src/char/BaseChar.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9506fd4fd0505b8f9ea36d8cf312852723aad6ad4b6f12cb7db6c5e17bdc6ead
-size 40878
+oid sha256:32c61f63988e1e96bb4308b32e09e9e05336db722fb784ff1b77d7e4e88d081a
+size 41121
