3b29fb6a75c7da26039d6a28a542ffb9c919bb47
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-05-28 19:36:57 +0800

    修复71版本小问题

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4796af540c292e7ecc210fd044d24c85abb191b61d02ebf4582219ef434b5fdf
-size 33237
+oid sha256:6fedd21dbf31b2c7a5c058d77f8e50b72685c9a8c0004ea243d93854efac8954
+size 33295
