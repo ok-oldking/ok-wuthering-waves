d54942b93b357fa253b219733962c1b2ce1c3c48
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-05-28 18:56:24 +0800

    https://www.paypal.com/ncp/payment/JWQBH7JZKNGCQ

--- src/task/BaseCombatTask.py
+++ src/task/BaseCombatTask.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5df94b6c9d16fd66f6b26da64006eb2ad92dd50f3fb01c3b734a9ef280c7f7a
-size 31730
+oid sha256:ead3f443140e0ae182b1cc4d90eb563a7453a30913dd5f76c549a462718fb1bc
+size 31813
