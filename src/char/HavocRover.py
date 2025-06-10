7454406dda01e4af3ea809624d42724c235b59bf
RU <cherish870424@gmail.com>
2025-06-10 15:30:54 +0800

    优化暗主

--- src/char/HavocRover.py
+++ src/char/HavocRover.py
@@ -1,20 +1,27 @@
+import time
 from src.char.BaseChar import BaseChar, Elements
 
 
 class HavocRover(BaseChar):
     def __init__(self, *args, **kwargs):
+        self.liber_available = False
         super().__init__(*args, **kwargs)
 
     def reset_state(self):
         self.ring_index = -1
+        self.liber_available = False
         super().reset_state()
 
     def do_perform(self):
         if self.ring_index == -1:
             self.task._ensure_ring_index()
+        if not self.has_intro:
+            self.sleep(0.01)
         if self.ring_index == Elements.HAVOC:
+            self.intro_motion_freeze_duration = 0.64
             self.perform_havoc_routine()
         elif self.ring_index == Elements.SPECTRO:
+            self.intro_motion_freeze_duration = 0.92
             self.perform_spectro_routine()
         else:
             self.perform_basic_routine()
@@ -22,7 +29,8 @@ class HavocRover(BaseChar):
 
     def perform_spectro_routine(self):
         if self.has_intro:
-            self.wait_intro()
+            self.continues_normal_attack(self.intro_motion_freeze_duration)
+        self.wait_down()
         self.aftertune_combo()
         if self.echo_available():
             self.click_echo(time_out=0.2)
@@ -46,22 +54,32 @@ class HavocRover(BaseChar):
 
     def perform_havoc_routine(self):
         if self.has_intro:
-            self.wait_intro()
-        if self.liberation_available():
+            self.continues_normal_attack(self.intro_motion_freeze_duration)
+        if self.liber_available or self.liberation_available():
             if self.is_forte_full():
-                if self.resonance_available():
-                    self.click_resonance(send_click=True, post_sleep=0.75)
-                self.heavy_attack(0.8)
+                self.check_combat()
+                self.task.mouse_down()
+                start = time.time()
+                while True:
+                    current = time.time()
+                    if current - start > 0.5 and not self.is_forte_full() or current - start > 2:
+                        break
+                    self.task.next_frame()
+                self.task.mouse_up()
+            else:
+                self.wait_down()
             self.click_liberation(send_click=True)
+        self.wait_down()
         if self.click_resonance(send_click=True)[0]:
+            self.liber_available = self.liberation_available()
             return
         if not self.click_echo():
             self.click()
-        self.continues_normal_attack(1.1 - self.time_elapsed_accounting_for_freeze(self.last_switch_time))
+        self.continues_normal_attack(0.9)
+        self.liber_available = self.liberation_available()
 
     def perform_basic_routine(self):
-        if self.has_intro:
-            self.continues_normal_attack(self.intro_motion_freeze_duration)
+        self.wait_intro()
         if self.echo_available():
             self.click_echo(time_out=0.2)
         liber = self.click_liberation(send_click=True)
