860f79490790cdcc91246b4ab687528e89499967
RU <cherish870424@gmail.com>
2025-06-12 15:05:15 +0800

    优化卡提

--- src/char/Cartethyia.py
+++ src/char/Cartethyia.py
@@ -4,11 +4,21 @@ from src.char.BaseChar import BaseChar, Priority
 
 
 class Cartethyia(BaseChar):
+    def __init__(self, *args, **kwargs):
+        self.manifest_time = -1
+        self.is_cartethyia = True
+        super().__init__(*args, **kwargs)
+
+    def reset_state(self):
+        self.manifest_time = -1
+        super().reset_state()
+
     def do_perform(self):
         if self.has_intro:
             self.continues_normal_attack(1.2)
         if self.is_small():
-            self.click_liberation()
+            if self.click_liberation():
+                self.manifest_time = time.time() if self.manifest_time < 0 else self.manifest_time
         if self.click_resonance()[0]:
             pass
         elif self.is_small() and self.click_echo():
@@ -25,14 +35,18 @@ class Cartethyia(BaseChar):
         self.switch_next_char()
 
     def is_small(self):
-        return self.task.find_one('forte_cartethyia_space')
+        self.is_cartethyia = self.task.find_one('forte_cartethyia_space')
+        return self.is_cartethyia
 
     def do_get_switch_priority(self, current_char: BaseChar, has_intro=False, target_low_con=False):
-        return Priority.MAX - 2
+        if not self.is_cartethyia:
+            return Priority.MAX - 2
+        return super().do_get_switch_priority(current_char, has_intro)
 
     def try_lib_big(self):
         if self.is_lib_big_available():
             if self.click_liberation():
+                self.manifest_time = -1
                 self.click_resonance()
                 return True
 
