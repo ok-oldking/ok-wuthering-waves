3cc5b15c660f4b884044b7fb3d30ac0c6c8d52f6
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-06-16 00:40:57 +0800

    修复5合1 勾选全部属性时会全部合成 fix #608

--- src/task/FiveToOneTask.py
+++ src/task/FiveToOneTask.py
@@ -72,6 +72,9 @@ class FiveToOneTask(BaseCombatTask):
 
     def merge_set(self, name_box, set_name, step):
         keeps = self.config.get(set_name, [])
+        if len(keeps) == 16:
+            self.log_info('保存所有, 跳过')
+            return
         if step == 2 and "攻击力百分比" not in keeps:  # 4C攻击力
             self.log_info("没有选择攻击力百分比, 跳过第二步")
             return
