23a3de2bbcfd1a459ce3f1bb206d4bce95c2c992
medicinemelancholy <1807904567@qq.com>
2025-06-16 21:30:50 +0800

    添加pc网页端云鸣潮支持

---
+++ main_cloud.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    from config import config
+    from ok import OK
+    from ok import ConfigOption
+
+    config = config
+    del config['windows']['hwnd_class']
+    del config['windows']['calculate_pc_exe_path']
+    config['windows']['exe'] = ['chrome.exe', 'msedge.exe']
+        'Color for Cloud Wuwa': True
+        }, description='Setting for Cloud Wuwa', config_description={
+        'Color for Cloud Wuwa': 'Color in Cloud Wuwa may be unreal. Turn on for adjust the color setting'
+    config['global_configs'][3] = cloud_config_option
+    ok = OK(config)
+    ok.start()
