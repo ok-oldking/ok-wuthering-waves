62ca921731e3715edaed6080615afd094955282a
medicinemelancholy <1807904567@qq.com>
2025-06-16 21:55:41 +0800

    添加pc网页端云鸣潮支持

--- config.py
+++ config.py
@@ -67,13 +67,19 @@ monthly_card_config_option = ConfigOption('Monthly Card Config', {
     'Monthly Card Time': 'Your computer\'s local time when the monthly card will popup, hour in (1-24)'
 })
 
+cloud_config_option = ConfigOption('Cloud Config', {
+    'Color for Cloud Wuwa': False
+}, description='Setting for Cloud Wuwa', config_description={
+    'Color for Cloud Wuwa': 'Color in Cloud Wuwa may be unreal. Turn on for adjust the color setting'
+})
+
 config = {
     'debug': False,  # Optional, default: False
     'use_gui': True,
     'config_folder': 'configs',
     'screenshot_processor': make_bottom_right_black,
     'gui_icon': 'icon.png',
-    'global_configs': [key_config_option, pick_echo_config_option, monthly_card_config_option],
+    'global_configs': [key_config_option, pick_echo_config_option, monthly_card_config_option, cloud_config_option],
     'ocr': {
         'lib': 'onnxocr'
     },
---
+++ main_cloud.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    from config import config
+    from ok import OK
+    from ok import ConfigOption
+
+    config = config
+    del config['windows']['hwnd_class']
+    del config['windows']['calculate_pc_exe_path']
+
+    config['windows']['exe'] = ['chrome.exe', 'msedge.exe']
+    cloud_config_option = ConfigOption('Cloud Config', {
+        'Color for Cloud Wuwa': True
+        }, description='Setting for Cloud Wuwa', config_description={
+        'Color for Cloud Wuwa': 'Color in Cloud Wuwa may be unreal. Turn on for adjust the color setting'
+    })
+    config['global_configs'][3] = cloud_config_option
+    ok = OK(config)
+    ok.start()
