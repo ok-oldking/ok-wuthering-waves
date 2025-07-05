1bb78428dcae6166498a5ccf95e244f2f65bd80a
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2025-07-05 22:24:46 +0800

    add enhanced feature of forgery task

--- config.py
+++ config.py
@@ -166,6 +166,7 @@ config = {
         ["src.task.DailyTask", "DailyTask"],
         ["src.task.FarmEchoTask", "FarmEchoTask"],
         ["src.task.FiveToOneTask", "FiveToOneTask"],
+        ["src.task.ForgeryTask", "ForgeryTask"],
         ["src.task.SimulationTask", "SimulationTask"],
         ["src.task.TacetTask", "TacetTask"],
         ["src.task.DiagnosisTask", "DiagnosisTask"],
