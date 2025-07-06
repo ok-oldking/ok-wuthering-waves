65e83e5799a5c9d692c3d6f8db95157a86546ad4
Zhicheng Zhang <20331014+zzc-tongji@users.noreply.github.com>
2025-07-06 11:06:35 +0800

    fix a bug happend when not enough stamina

--- src/task/SimulationTask.py
+++ src/task/SimulationTask.py
@@ -44,6 +44,7 @@ class SimulationTask(DomainTask):
         current, back_up = self.open_F2_book_and_get_stamina()
         if (current + back_up < self.stamina_once):
             self.back()
+            return
         self.teleport_into_domain(self.config.get('Material Selection', 'Shell Credit'))
         self.farm_in_domain(total_counter=total_counter, current=current, back_up=back_up)
 
