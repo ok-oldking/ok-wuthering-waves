afb10f96f7968334c2f0bc0d3cd8cb37d7961eff
medicinemelancholy <1807904567@qq.com>
2025-07-27 17:11:48 +0800

    添加fll延奏逻辑
    吃到坎延奏时站场打长轴

--- src/char/Phrolova.py
+++ src/char/Phrolova.py
@@ -14,18 +14,23 @@ class Phrolova(BaseChar):
     def do_perform(self):
         self.last_liberation = -1
         if self.has_intro:
-            self.continues_normal_attack(0.5)
+            if self.check_outro() in {'char_cantarella'}:
+                self.do_perform_outro()
+                return self.switch_next_char()
+            self.continues_normal_attack(0.8)
         if self.flying():
             self.wait_down()
         if self.click_liberation():
             return self.switch_next_char()
-        elif self.heavy_and_liber():           
+        if self.heavy_and_liber():           
             return self.switch_next_char()
         self.continues_normal_attack(3, click_resonance_if_ready_and_return=True)
         self.click_echo()
         self.switch_next_char()
 
     def do_get_switch_priority(self, current_char: BaseChar, has_intro=False, target_low_con=False):
+        if self.time_elapsed_accounting_for_freeze(self.last_liberation) > 8 and has_intro and current_char.char_name in {'char_cantarella'}:
+            return Priority.MAX
         if self.time_elapsed_accounting_for_freeze(self.last_liberation) < 24:
             return Priority.MIN
         return Priority.FAST_SWITCH
@@ -38,3 +43,21 @@ class Phrolova(BaseChar):
             self.logger.debug('Phrolova heavy_click_forte')
             self.task.wait_until(self.click_liberation, time_out=3)
             return True
+            
+    def do_perform_outro(self):
+        if self.flying():
+            self.wait_down()
+        if self.click_liberation():
+            return
+        self.continues_normal_attack(0.5)
+        while self.time_elapsed_accounting_for_freeze(self.last_perform) < 16:
+            if self.click_liberation():
+                return
+            if self.heavy_and_liber():           
+                return self.switch_next_char()
+            self.click_echo()  
+            if self.resonance_available():
+                self.click_resonance()
+            self.task.click()
+            self.check_combat()
+            self.task.next_frame()
