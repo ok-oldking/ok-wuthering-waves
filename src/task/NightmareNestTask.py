736f37ecc03a3b317c08d9965f3c7a9c68f8f9c0
medicinemelancholy <1807904567@qq.com>
2025-08-25 22:18:25 +0800

    fix picking echoes

--- src/task/NightmareNestTask.py
+++ src/task/NightmareNestTask.py
@@ -36,7 +36,25 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
 
     def run(self):
         WWOneTimeTask.run(self)
-        self.log_info(r'36/36')
+        test_picking_echo = False
+        if test_picking_echo:
+            chance = True
+            circle = 0
+            while True:
+                dropped, has_more = self.yolo_find_echo(turn=True, use_color=False, time_out=12, threshold=0.25)
+                self.sleep(0.5)
+                circle += 1
+                if not dropped and not has_more:
+                    if chance:
+                        self.log_info('find echo failed, have another chance')
+                        chance = False
+                    else:
+                        self.log_info('find echo failed, tele to the next')
+                        break
+                else:
+                    chance = True
+                if circle >= 30:
+                    break
         for echo_name in self.find_echo_list():
             self.ensure_main(time_out=180)
             self.openF2Book("gray_book_quest")
@@ -56,7 +74,7 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
                 self.log_info(f'{echo_name} is complete')
                 continue
             self.click(0.89, 0.92)
-            self.wait_in_team_and_world(time_out=120)
+            self.wait_in_team_and_world(raise_if_not_found=False)
             self.wait_until(self.in_combat,post_action=self.middle_click, time_out=10)
             if self.in_combat():
                 self.log_info('wait combat')
@@ -67,10 +85,12 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
             self.log_info('find echo')
             self.pick_echo()
             chance = True
+            circle = 0
             while True:
                 dropped, has_more = self.yolo_find_echo(turn=True, use_color=False, time_out=12, threshold=0.25)
                 self.sleep(0.5)
-                if not dropped or not has_more:
+                circle += 1
+                if not dropped and not has_more:
                     if chance:
                         self.log_info('find echo failed, have another chance')
                         chance = False
@@ -79,6 +99,8 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
                         break
                 else:
                     chance = True
+                if circle >= 30:
+                    break
         self.ensure_main(time_out=180)
         self.log_info(f'NightmareNestTask complete')
                   
@@ -104,3 +126,46 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
             self.sleep(0.2)
             return True
         return False
+        
+    def walk_to_yolo_echo(self, time_out=8, update_function=None, echo_threshold=0.5):
+        last_direction = None
+        start = time.time()
+        no_echo_start = 0
+        while time.time() - start < time_out:
+            self.next_frame()
+            if self.find_one('pick_up_f_hcenter_vcenter', box=self.f_search_box, threshold=0.8):
+                self._stop_last_direction(last_direction)
+                if self.pick_f():
+                    self.log_debug('pick echo success')
+                return True
+            if self.in_combat():
+                self.log_debug('pick echo has_target return fail')
+                self._stop_last_direction(last_direction)
+                return False
+            echos = self.find_echos(threshold=echo_threshold)
+            if not echos:
+                if no_echo_start == 0:
+                    no_echo_start = time.time()
+                elif time.time() - no_echo_start > 3:
+                    self.log_debug(f'walk front to_echo, no echos found, have another chance')
+                    self._stop_last_direction(last_direction)
+                    return True
+                next_direction = 'w'
+            else:
+                no_echo_start = 0
+                echo = echos[0]
+                center_distance = echo.center()[0] - self.width_of_screen(0.5)
+                threshold = 0.05 if not last_direction else 0.15
+                if abs(center_distance) < self.height_of_screen(threshold):
+                    if echo.y + echo.height > self.height_of_screen(0.65):
+                        next_direction = 's'
+                    else:
+                        next_direction = 'w'
+                elif center_distance > 0:
+                    next_direction = 'd'
+                else:
+                    next_direction = 'a'
+            last_direction = self._walk_direction(last_direction, next_direction)
+            if update_function is not None:
+                update_function()
+        self._stop_last_direction(last_direction)
