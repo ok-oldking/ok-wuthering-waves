24098dd1ab4d89f2e180273be481b32c484ffc78
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-08-26 18:45:08 +0800

    优化刷梦魇聚落

--- src/task/NightmareNestTask.py
+++ src/task/NightmareNestTask.py
@@ -15,6 +15,7 @@ from src.task.WWOneTimeTask import WWOneTimeTask
 
 logger = Logger.get_logger(__name__)
 
+
 class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
 
     def __init__(self, *args, **kwargs):
@@ -61,11 +62,7 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
             self.click(0.04, 0.53)
             self.wait_hint(0.05, 0.04, 0.12, 0.08, r'敌迹探寻')
             self.click(0.13, 0.14, after_sleep=0.5)
-            for char in echo_name:
-                win32api.SendMessage(self.hwnd.hwnd, 
-                     win32con.WM_CHAR, 
-                     ord(char), 0)
-                self.sleep(0.01)
+            self.input_text(echo_name)
             self.click(0.39, 0.13, after_sleep=0.5)
             self.click(0.13, 0.24, after_sleep=0.5)
             self.click(0.89, 0.92)
@@ -75,7 +72,7 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
                 continue
             self.click(0.89, 0.92)
             self.wait_in_team_and_world(raise_if_not_found=False)
-            self.wait_until(self.in_combat,post_action=self.middle_click, time_out=10)
+            self.wait_until(self.in_combat, post_action=self.middle_click, time_out=10)
             if self.in_combat():
                 self.log_info('wait combat')
                 self.combat_once(wait_combat_time=0, raise_if_not_found=False)
@@ -103,16 +100,15 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
                     break
         self.ensure_main(time_out=180)
         self.log_info(f'NightmareNestTask complete')
-                  
+
     def find_echo_list(self):
         if self.game_lang == 'zh_CN':
             return self.echo_list_cn
         elif self.game_lang == 'zh_TW':
             return self.echo_list_tw
         return self.echo_list_us
-       
- 
-    def find_next_hint(self, x1, y1, x2, y2, s, box_name = 'hint_text'):
+
+    def find_next_hint(self, x1, y1, x2, y2, s, box_name='hint_text'):
         texts = self.ocr(box=self.box_of_screen(x1, y1, x2, y2, hcenter=True),
                          target_height=540, name=box_name)
         fps_text = find_boxes_by_name(texts,
@@ -121,12 +117,12 @@ class NightmareNestTask(WWOneTimeTask, BaseCombatTask):
             return True
         return False
 
-    def wait_hint(self, x1, y1, x2, y2, hint, timeout = 2):
+    def wait_hint(self, x1, y1, x2, y2, hint, timeout=2):
         if self.wait_until(lambda: self.find_next_hint(x1, y1, x2, y2, hint), time_out=timeout):
             self.sleep(0.2)
             return True
         return False
-        
+
     def walk_to_yolo_echo(self, time_out=8, update_function=None, echo_threshold=0.5):
         last_direction = None
         start = time.time()
