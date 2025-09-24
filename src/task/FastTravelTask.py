5814b2f1522d26c8eedc5d89acf3506a8b0c621a
bingxl <bingxl@outlook.com>
2025-09-24 10:26:35 +0800

    rename FastTravel to FastTravelTask

--- src/task/FastTravelTask.py
+++ src/task/FastTravelTask.py
@@ -5,21 +5,23 @@ from ok import TriggerTask, Logger
 logger = Logger.get_logger(__name__)
 
 
-class FastTravel(TriggerTask):
+class FastTravelTask(TriggerTask):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.default_config = {'_enabled': True}
+        self.default_config = {'_enabled': False}
         # self.trigger_interval = 0.5
         self.name = "Fast Travel"
+
+        # @TODO add more i18n support
+        self.match_re = re.compile(r'FastTravel|快速旅行')
 
     def run(self):
         # Fast Travel box position 1480,980, 230, 63 with 1920x1080
         self.log_debug("start run FastTravel.run")
         results = self.ocr(
             box=self.box_of_screen(0.77, 0.90, 0.88, 0.958, hcenter=True),
-            # @TODO add more i18n support
-            match=re.compile(r'FastTravel|快速旅行'),
+            match=self.match_re,
             # log=True,
             threshold=0.8)
     
