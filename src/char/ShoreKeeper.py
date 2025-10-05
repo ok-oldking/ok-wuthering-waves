8ab8929f4a80b205ac38f0d6fad08bbb667eb835
Xinweigh <zhaizixuan_1@foxmail.com>
2025-10-05 13:59:09 +0800

    优化守岸人技能顺序，避免先e后r过快导致e的治疗量丢失

--- src/char/ShoreKeeper.py
+++ src/char/ShoreKeeper.py
@@ -17,11 +17,12 @@ class ShoreKeeper(Healer):
             self.logger.debug('ShoreKeeper wait intro animation')
             # self.task.wait_in_team_and_world(time_out=4, raise_if_not_found=False)
             # self.check_combat()
-        self.click_resonance(send_click=False)        
-        self.liberation_available() and self.wait_down()
-        self.click_liberation()
-        self.click_echo(time_out=0)
+        if self.liberation_available():
+            self.wait_down()
+            self.click_liberation()
         self.heavy_click_forte(check_fun = self.is_mouse_forte_full)
+        self.click_resonance(send_click=False, animation_min_duration=1)
+        self.click_echo(time_out=0)
         self.switch_next_char()
 
     def switch_next_char(self, *args):
