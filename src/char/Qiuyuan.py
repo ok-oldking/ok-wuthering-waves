56e7d895436e23b542a433907684de9d1e63ec1b
medicinemelancholy <1807904567@qq.com>
2025-10-31 00:02:01 +0800

    支持仇遠

--- src/char/Qiuyuan.py
+++ src/char/Qiuyuan.py
@@ -1,7 +1,34 @@
+import time
+
 from src.char.BaseChar import BaseChar
 
 
 class Qiuyuan(BaseChar):
-
     def do_perform(self):
-        pass
+        if self.has_intro:
+            self.continues_normal_attack(1.17)
+        if self.flying():
+            self.wait_down()            
+        start = time.time()
+        timeout = lambda: time.time() - start < 1.2
+        if self.has_intro:
+            timeout = lambda: time.time() - start < 4
+        while timeout():        
+            self.click_echo(time_out=0)
+            if time.time() - start < 0.5 and self.click_liberation():              
+                start = time.time()
+            if self.heavy_click_forte(check_fun=self.is_mouse_forte_full):
+                return self.switch_next_char()
+            if self.flying() and not self.is_mouse_forte_full():
+                self.shorekeeper_auto_dodge()  
+            self.click()
+            self.check_combat()
+            self.task.next_frame()
+        self.click_resonance()
+        self.switch_next_char()
+        
+    def shorekeeper_auto_dodge(self):
+        from src.char.ShoreKeeper import ShoreKeeper
+        for i, char in enumerate(self.task.chars):
+            if isinstance(char, ShoreKeeper):
+                return char.auto_dodge(condition = self.flying)
