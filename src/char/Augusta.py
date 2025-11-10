cb7e81da99d18bbc2879ed8feb1820fc91edbd90
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-11-10 20:49:26 +0800

    格式化代码

--- src/char/Augusta.py
+++ src/char/Augusta.py
@@ -1,10 +1,6 @@
 import time
-import re
 
-from ok import find_boxes_by_name
 from src.char.BaseChar import BaseChar
-from src.task.BaseWWTask import binarize_for_matching
-from src import text_white_color
 
 """ 
     几个长派生帧动作的切人时间阈值,改小可以减少站场时间
@@ -12,37 +8,38 @@ from src import text_white_color
 """
 switch_time = 3
 
+
 class Augusta(BaseChar):
     def do_perform(self):
         if self.has_intro:
             self.continues_normal_attack(1.13)
         if self.flying():
-            self.wait_down() 
+            self.wait_down()
         start = time.time()
         timeout = lambda: time.time() - start < 6
-        while timeout():              
-            if self.check_majesty() :
+        while timeout():
+            if self.check_majesty():
                 self.logger.debug('Augusta performs majesty')
                 if self.perform_majesty():
                     self.send_echo_key()
                     return self.switch_next_char()
             if self.flying():
                 self.shorekeeper_auto_dodge()
-            if self.check_prowess() and self.perform_prowess():                
+            if self.check_prowess() and self.perform_prowess():
                 if time.time() - start > switch_time:
                     return self.switch_next_char()
             if self.resonance_available():
-                self.logger.debug('Augusta performs single resonance') 
+                self.logger.debug('Augusta performs single resonance')
                 now = time.time()
                 self.click_resonance()
-                self.logger.debug(f'time = {time.time() - now}') 
+                self.logger.debug(f'time = {time.time() - now}')
                 if time.time() - now < 1.4:
                     if self.flying():
                         continue
                     if self.task.wait_until(self.check_prowess, time_out=1) and self.perform_prowess():
                         if time.time() - start > switch_time and not self.flying():
                             return self.switch_next_char()
-                else: 
+                else:
                     if self.check_majesty():
                         self.wait_down()
                         if self.perform_majesty():
@@ -50,23 +47,24 @@ class Augusta(BaseChar):
                     return self.switch_next_char()
             if self.liberation_available():
                 self.logger.debug('Augusta performs single liberation')
-                if self.task.wait_until(lambda: not self.liberation_available(), post_action=self.send_liberation_key, time_out=2):
+                if self.task.wait_until(lambda: not self.liberation_available(), post_action=self.send_liberation_key,
+                                        time_out=2):
                     self.update_liberation_cd()
-                    return self.switch_next_char()     
+                    return self.switch_next_char()
             self.click()
             self.check_combat()
-            self.task.next_frame()  
+            self.task.next_frame()
         self.send_echo_key()
-        self.switch_next_char()  
-        
+        self.switch_next_char()
+
     def perform_prowess(self):
-        self.logger.debug('Augusta performs prowess') 
+        self.logger.debug('Augusta performs prowess')
         if not self.heavy_click_forte(self.check_prowess):
             return False
         self.continues_normal_attack(0.3)
         return True
-    
-    def perform_majesty(self, time_out=0.6, wait_down = False):        
+
+    def perform_majesty(self, time_out=0.6, wait_down=False):
         self.task.send_key_down(self.get_liberation_key())
         self.task.in_liberation = True
         if wait_down:
@@ -80,34 +78,34 @@ class Augusta(BaseChar):
             self.task.in_liberation = False
             return False
         self.task.wait_until(lambda: self.task.in_team()[0], post_action=self.click, time_out=10)
-        self.add_freeze_duration(start, time.time()-start)         
-        self.logger.info(f'click_liberation end {time.time()-start}')
-        
+        self.add_freeze_duration(start, time.time() - start)
+        self.logger.info(f'click_liberation end {time.time() - start}')
+
         return True
-            
+
     def check_ascendancy(self):
         return False
-    
+
     def liberation_available(self):
         return self.current_liberation() > 0 and bool(self.task.find_one('Augusta_lib1', threshold=0.6))
-            
+
     def check_majesty(self):
         return self.current_liberation() > 0 and bool(self.task.find_one('Augusta_lib2', threshold=0.6))
-     
+
     def check_prowess(self):
         long_inner_box = 'box_target_enemy_long_inner'
         if self.task.find_one(long_inner_box, box=self.task.get_box_by_name(long_inner_box), threshold=0.85):
             return True
-    
+
     def resonance_available(self):
         return not self.has_cd('resonance')
-        
+
     def shorekeeper_auto_dodge(self):
         from src.char.ShoreKeeper import ShoreKeeper
         for i, char in enumerate(self.task.chars):
             if isinstance(char, ShoreKeeper):
-                return char.auto_dodge(condition = self.flying)   
-                
+                return char.auto_dodge(condition=self.flying)
+
     def count_echo_priority(self):
         return 0
 
@@ -115,4 +113,3 @@ class Augusta(BaseChar):
         next_char = str((self.index + 1) % len(chars) + 1)
         self.logger.debug(f'Augusta on_combat_end {self.index} switch next char: {next_char}')
         self.task.send_key(next_char)
- 
