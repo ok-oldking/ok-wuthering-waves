fe22f27b37eee3a8ce76c1bc15310619e10ba4dc
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-01-08 19:57:20 +0800

    优化多个屏幕支持, 优化热更重启游戏逻辑

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -686,7 +686,7 @@ class BaseWWTask(BaseTask):
                 return False
             if self.find_boxes(texts, match=re.compile("游戏即将重启")):                
                 self.log_info('游戏更新成功, 游戏即将重启')
-                self.click(self.find_boxes(texts, match="确认"), after_sleep=5)
+                self.click(self.find_boxes(texts, match="确认"), after_sleep=20)
                 result = self.start_device()
                 self.log_info(f'start_device end {result}')
                 self.sleep(20)
