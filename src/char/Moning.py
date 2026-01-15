986d17138d61e5979d50264aaf9ee6d03d255837
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-01-16 01:46:22 +0800

    优化莫宁

--- src/char/Moning.py
+++ src/char/Moning.py
@@ -15,11 +15,12 @@ class Moning(Healer):
                 self.sleep(0.1)  
                 if self.is_mouse_forte_full():
                     self.heavy_attack()
-                    self.sleep(0.5)
+                    self.sleep(0.3)                    
         else:
             self.logger.debug('already on_air')
         if self.on_air():  
             self.logger.debug('on_air start attacking')
+            self.click_liberation()
             start = time.time()
             time_out = 10
             while time.time() - start < time_out and not self.is_mouse_forte_full():
@@ -29,8 +30,6 @@ class Moning(Healer):
             if self.is_mouse_forte_full():
                 self.logger.debug('mouse forte full, heavy attack')
                 self.heavy_attack()
-                self.sleep(0.5)
-                self.click_liberation()
         else:
             self.logger.debug('failed to jump on_air, switch next char')
         self.switch_next_char()
