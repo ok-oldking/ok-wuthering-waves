2195a63d0f25be6b3a8eb8cd5f23d67a0d30a239
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-01-15 23:01:47 +0800

    Add Moning character

--- src/char/CharFactory.py
+++ src/char/CharFactory.py
@@ -23,6 +23,7 @@ from src.char.Jiyan import Jiyan
 from src.char.Linnai import Linnai
 from src.char.Lupa import Lupa
 from src.char.Mortefi import Mortefi
+from src.char.Moning import Moning
 from src.char.Phoebe import Phoebe
 from src.char.Phrolova import Phrolova
 from src.char.Qiuyuan import Qiuyuan
@@ -92,6 +93,7 @@ char_dict = {
                    'ring_index': Elements.HAVOC},
     'char_douling': {'cls': Douling, 'res_cd': 15, 'echo_cd': 25, 'liberation_cd': 25, 'ring_index': Elements.ELECTRIC},
     'char_linnai': {'cls': Linnai, 'res_cd': 15, 'echo_cd': 25, 'liberation_cd': 25, 'ring_index': Elements.SPECTRO},
+    'char_moning': {'cls': Moning, 'res_cd': 15, 'echo_cd': 25, 'liberation_cd': 25, 'ring_index': Elements.FIRE},
 }
 
 char_names = char_dict.keys()
---
+++ src/char/Moning.py
@@ -0,0 +1,5 @@
+from src.char.Healer import Healer
+
+
+class Moning(Healer):
+    pass
