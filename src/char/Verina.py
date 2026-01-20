edecfe94f41dd51be8cf343324df7d1e81b197f3
Me1onpan <me1onpan@outlook.com>
2026-01-20 22:58:46 +0800

    refactor(verina): 优化战斗技能释放逻辑
    - 调整技能释放顺序，优先释放共鸣和声骸
    - 精细化技能参数配置，缩短重击持续时间
    - 改进重击释放机制，增加跳跃判断

--- src/char/Verina.py
+++ src/char/Verina.py
@@ -11,15 +11,29 @@ class Verina(Healer):
         return 2
 
     def do_perform(self):
+        self.wait_down(click=False)
+
         if self.has_intro:
-            self.heavy_attack(1.5)
+            self.heavy_attack(duration=0.05)
+            self.wait_down(click=False)
+        else:
+            self.continues_normal_attack(1.2)
+
+        if self.resonance_available():
+            self.click_resonance(send_click=False, post_sleep=0.375)
+        if self.echo_available():
+            self.click_echo()
+        if self.liberation_available():
+            self.click_liberation()
             return self.switch_next_char()
-        self.wait_down()
-        liberated = self.click_liberation()
-        self.click_resonance(send_click=False)
-        self.click_echo(time_out=0)
+
         if self.is_mouse_forte_full():
-            self.heavy_attack()
-        elif not liberated:
-            self.click_liberation(wait_if_cd_ready=1)
-        self.switch_next_char()
+            count = 0
+            while not self.flying():
+                self.task.jump()
+                count += 1
+                if count >= 10:
+                    return self.switch_next_char()
+            self.continues_normal_attack(0.8)
+
+        return self.switch_next_char()
