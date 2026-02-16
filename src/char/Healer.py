277ecdb7e1b9bf82f8e2f61f8a208108a45826d3
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-02-16 12:11:47 +0800

    调整治疗切人优先级

--- src/char/Healer.py
+++ src/char/Healer.py
@@ -4,10 +4,13 @@ from src.char.BaseChar import BaseChar, Priority
 class Healer(BaseChar):
 
     def do_get_switch_priority(self, current_char, has_intro=False, target_low_con=False):
-        if not has_intro and self.liberation_available():
+        if self.time_elapsed_accounting_for_freeze(self.last_perform) > 20:
+            self.logger.debug(
+                f'Healer do_get_switch_priority 20 seconds since last switch')
+            return Priority.SKILL_AVAILABLE * 2
+        elif not has_intro and self.liberation_available() and self.resonance_available() and self.echo_available():
             self.logger.debug(
                 f'Healer do_get_switch_priority everything available')
             return Priority.SKILL_AVAILABLE * 2
         else:
             return Priority.BASE_MINUS_1
-        
