50fd831b6be81a28cd3b5eb43539246dbc49275f
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-02-19 09:10:55 +0800

    回滚莫宁

--- src/char/Mornye.py
+++ src/char/Mornye.py
@@ -11,49 +11,62 @@ class Mornye(BaseChar):
         if self.has_intro:
             self.continues_normal_attack(1.33)
         self.check_f_on_switch = True
-        time_out = 10
-        start = time.time()
         if not self.on_air():
             if self.combo_limit():
-                self.logger.debug('perform quick actions')
-                if not self.click_echo():
+                self.logger.debug("perform quick actions")
+                if self.click_echo():
+                    return self.switch_next_char()
+                elif self.click_resonance()[0]:
+                    """地面奶一下，防止主C死亡"""
+                    return self.switch_next_char()
+                else:
                     self.continues_normal_attack(0.1)
-                return self.switch_next_char()
-            self.logger.debug('not on_air start attacking')
-            while time.time() - start < time_out and not self.on_air():
-                self.click()
-                self.click_resonance()
-                self.check_combat()
-                self.sleep(0.1)
-                if self.is_mouse_forte_full():
-                    self.heavy_attack()
-                    self.sleep(0.3)
-        else:
-            self.logger.debug('already on_air')
+                    return self.switch_next_char()
+            self.not_on_air_actions()
+
         if self.on_air():
-            self.logger.debug('on_air start attacking')
-            self.click_liberation()
-            start = time.time()
-            time_out = 10
-            while time.time() - start < time_out and not self.is_mouse_forte_full() and self.on_air():
-                self.click()
-                self.click_resonance()
-                self.check_combat()
-                self.sleep(0.1)
-            if self.on_air() and self.is_mouse_forte_full():
-                self.logger.debug('mouse forte full, heavy attack')
-                if self.heavy_click_forte(check_fun=self.is_mouse_forte_full):
-                    self.last_heavy = time.time()
-                    self.check_f_on_switch = False
-        else:
-            self.logger.debug('failed to jump on_air, switch next char')
+            self.on_air_actions()
+
         self.switch_next_char()
 
     def on_air(self):
         return self.has_long_action2()
 
+    def on_air_actions(self):
+        self.logger.debug("on_air start attacking")
+        start = time.time()
+        while (
+                time.time() - start < 10
+                and self.on_air()
+                and not self.is_mouse_forte_full()
+        ):
+            self.click_liberation()
+            self.click_resonance()
+            self.click()
+            self.check_combat()
+            self.sleep(0.1)
+        """可能会被打断，或者强制落地，这时候就不继续重置重击了"""
+        if self.on_air() and self.is_mouse_forte_full():
+            self.logger.debug("mouse forte full, heavy attack")
+            if self.heavy_click_forte(check_fun=self.is_mouse_forte_full):
+                self.last_heavy = time.time()
+                self.check_f_on_switch = False
+        self.logger.debug("finished attacking on_air")
+
+    def not_on_air_actions(self):
+        self.logger.debug("not on_air start attacking")
+        start = time.time()
+        while time.time() - start < 10 and not self.on_air():
+            self.click()
+            self.click_resonance()
+            self.check_combat()
+            self.sleep(0.1)
+            if self.is_mouse_forte_full():
+                self.heavy_attack()
+                self.sleep(0.3)
+
     def on_combat_end(self, chars):
         self.switch_other_char()
 
     def combo_limit(self):
-        return self.time_elapsed_accounting_for_freeze(self.last_heavy) < 20
+        return self.time_elapsed_accounting_for_freeze(self.last_heavy) < 23
