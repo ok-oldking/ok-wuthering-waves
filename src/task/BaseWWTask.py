1cba991ff20aaa5e19da426894f485e5cc613520
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-03-13 19:24:59 +0800

    适配月卡位置移动

--- src/task/BaseWWTask.py
+++ src/task/BaseWWTask.py
@@ -879,10 +879,10 @@ class BaseWWTask(BaseTask):
         # Function to check if a component forms a ring
 
     def find_monthly_card(self):
-        return self.find_one('monthly_card', threshold=0.8, vertical_variance=0.07)
+        return self.find_one('monthly_card', threshold=0.8)
 
     def handle_monthly_card(self):
-        monthly_card = self.find_monthly_card
+        monthly_card = self.find_monthly_card()
         # self.screenshot('monthly_card1')
         if monthly_card is not None:
             # self.screenshot('monthly_card1')
