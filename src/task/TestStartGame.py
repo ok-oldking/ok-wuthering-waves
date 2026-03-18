4a2a03c7c4bab6015f32569f4cc69579bc71109a
AliceJump <2630923991@qq.com>
2026-03-18 17:28:10 +0800

    更改sleep为框架自带的sleep

--- src/task/TestStartGame.py
+++ src/task/TestStartGame.py
@@ -14,7 +14,7 @@ class TestStartGame(BaseWWTask):
             self.ensure_main(time_out=120)
             wait_time = self.config.get("回到主页后等待的时间", 15)
             self.log_info(f"成功启动游戏,等待{wait_time}s后自动关闭(可禁用)", notify=True)
-            time.sleep(wait_time)
+            self.sleep(wait_time)
         except TaskDisabledException:
             raise
         except Exception:
