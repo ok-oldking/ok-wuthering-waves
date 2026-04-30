5fa37c9425adc77fbd88c879a03be0c3ef35e4fd
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-04-30 20:01:31 +0800

    fix tests

--- tests/TestForte.py
+++ tests/TestForte.py
@@ -14,17 +14,11 @@ class TestForte(TaskTestCase):
         self.task.do_reset_to_false()
         self.set_image('tests/images/treasure2.png')
         self.task.load_chars()
-        self.assertIsNotNone(self.task.find_mouse_forte())
-
-    def test_forte2(self):
-        self.task.do_reset_to_false()
-        self.set_image('tests/images/test_forte.png')
-        self.task.load_chars()
-        self.assertIsNotNone(self.task.find_mouse_forte())
+        self.assertIsNone(self.task.find_mouse_forte())
 
     def test_forte3(self):
         self.task.do_reset_to_false()
-        self.set_image('tests/images/echo.png')
+        self.set_image('ok_templates/3.png')
         self.task.load_chars()
         self.assertIsNotNone(self.task.find_mouse_forte())
 
