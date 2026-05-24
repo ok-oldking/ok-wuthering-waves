fb3830ba11fc75f11e1d460b023021a71034494d
Jasonnor <wujason810@gmail.com>
2026-05-24 17:34:52 +0800

    feature: handle Denia black form state transitions

--- src/char/Denia.py
+++ src/char/Denia.py
@@ -6,23 +6,29 @@ class Denia(BaseChar):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.check_f_on_switch = False
+        self.is_black_form = False
+
+    def reset_state(self):
+        super().reset_state()
+        self.is_black_form = False
 
     def do_perform(self):
         if self.has_intro:
             self.continues_normal_attack(0.97)
-            self.click_resonance()
-            self.click_echo(time_out=0)
+        if not self.is_black_form:
+            if self.resonance_available() and self.click_resonance()[0]:
+                pass
+            self.click_echo()
             if self.click_liberation():
+                self.is_black_form = True  # fall-through to black form below
+        if self.is_black_form:
+            self.click_echo()
+            if not self.liberation_available() and self.has_intro:
                 self.continues_normal_attack(2.9)
-                self.click_resonance()
-                self.click_liberation()
-        else:
             if self.resonance_available() and self.click_resonance()[0]:
                 pass
             if self.click_liberation():
-                self.click_resonance()
-            self.click_echo()
-            
+                self.is_black_form = False
         self.switch_next_char()
 
     def must_switch(self, current_char=None, has_intro=False, target_low_con=False):
