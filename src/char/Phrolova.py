78b0b761fb51ffbcb87cfd0efcb49a5d7115f8f7
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-05-26 18:13:37 +0800

    弗洛洛开大后不在切

--- src/char/Phrolova.py
+++ src/char/Phrolova.py
@@ -76,10 +76,10 @@ class Phrolova(BaseChar):
 
     def get_switch_priority(self, current_char=None, has_intro=False, target_low_con=False):
         self.logger.debug(f'Phrolova last_liberation {self.time_elapsed_accounting_for_freeze(self.last_liberation)}')
-        if self._cantarella_outro_ready(current_char, has_intro):
-            return SwitchPriority.MUST
         if self.time_elapsed_accounting_for_freeze(self.last_liberation) < 24:
             return SwitchPriority.NO
+        if self._cantarella_outro_ready(current_char, has_intro):
+            return SwitchPriority.MUST
         return super().get_switch_priority(current_char, has_intro, target_low_con)
 
     def resonance_available(self):
