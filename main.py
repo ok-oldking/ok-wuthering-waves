d1bbd089e4eca33821212f56769b236160f03b71
Your Name <maxlinbaby@gmail.com>
2026-06-05 22:43:46 +0800

    修復：在 PySide6 import 前補上 PATH 環境變數，避免 KeyError

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+if 'PATH' not in os.environ:
+    os.environ['PATH'] = r'C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem'
+
 if __name__ == '__main__':
     from config import config
     from ok import OK
