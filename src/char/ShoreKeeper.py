347755f2d931d636f867574b005eb1dcb5f32b69
EEEEEEEEdison <106745928+EEEEEEEEdison@users.noreply.github.com>
2026-07-06 10:36:41 +0800

    Refactor ShoreKeeper by removing unused code
    Removed unused constants and methods related to intro liberation and Zani early empty cut blocking.

--- src/char/ShoreKeeper.py
+++ src/char/ShoreKeeper.py
@@ -4,9 +4,6 @@ from src.char.BaseChar import BaseChar, SwitchPriority
 
 
 class ShoreKeeper(BaseChar):
-    INTRO_READY_WAIT = 0.6
-    ZANI_EARLY_EMPTY_CUT_CON = 0.8
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.outrotime = -1
@@ -18,9 +15,6 @@ class ShoreKeeper(BaseChar):
         current_name = current_char.char_name if current_char else None
         if self.attribute == 2 and has_intro and current_name in {'Augusta', 'char_augusta'}:
             return SwitchPriority.MUST
-        if self._should_block_zani_early_empty_cut(current_char, has_intro):
-            self.logger.info("ShoreKeeper v5 block early empty cut during Zani first liberation")
-            return SwitchPriority.NO
         return super().get_switch_priority(current_char, has_intro, target_low_con)
 
     def skip_combat_check(self):
@@ -35,65 +29,7 @@ class ShoreKeeper(BaseChar):
         else:
             self.attribute = 1
 
-    def _fast_intro_liberation(self):
-        self.task.skip_combat_check = True
-        try:
-            self.task.wait_in_team_and_world(
-                time_out=self.INTRO_READY_WAIT,
-                raise_if_not_found=False,
-            )
-        finally:
-            self.task.skip_combat_check = False
-        return self.click_liberation()
-
-    def _should_fast_intro_liberation(self):
-        has_phoebe = False
-        has_zani = False
-        for char in self.task.chars:
-            if char is None:
-                continue
-            name = getattr(char, "char_name", "")
-            cls_name = char.__class__.__name__
-            if name == "char_phoebe" or cls_name == "Phoebe":
-                has_phoebe = True
-            elif name == "char_zani" or cls_name == "Zani":
-                has_zani = True
-        return has_phoebe and has_zani
-
-    def _should_block_zani_early_empty_cut(self, current_char, has_intro):
-        if has_intro or current_char is None:
-            return False
-        if not self._should_fast_intro_liberation():
-            return False
-        current_name = getattr(current_char, "char_name", "")
-        current_cls = current_char.__class__.__name__
-        if current_name != "char_zani" and current_cls != "Zani":
-            return False
-
-        current_con = 0
-        try:
-            current_con = current_char.get_current_con()
-        except Exception:
-            current_con = 0
-        if current_con < self.ZANI_EARLY_EMPTY_CUT_CON:
-            return False
-
-        if getattr(current_char, "in_liberation", False):
-            return True
-        if hasattr(current_char, "liberation_time_left") and current_char.liberation_time_left() > 0:
-            return True
-        if hasattr(current_char, "nightfall_time_left") and current_char.nightfall_time_left() > 0:
-            return True
-        return False
-
     def do_perform(self):
-        if (
-                self.has_intro
-                and self._should_fast_intro_liberation()
-                and self._fast_intro_liberation()
-        ):
-            return self.switch_next_char()
-
         if self.has_intro:
             self.task.skip_combat_check = True
             try:
