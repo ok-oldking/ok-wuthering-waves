f8553f77258c32f86d07ab6cb3bea19e46778ec4
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-08-02 10:11:15 +0800

    兼容21:9登录

--- config.py
+++ config.py
@@ -182,7 +182,7 @@ config = {
         'feature_processor': process_feature,
         'vcenter_features': ['monthly_card', 'skip_dialog_check'],
         'hcenter_features': ['monthly_card', 'suisui_forte3', 'message_dialog', 'claim_stamina_sign',
-                             'skip_dialog_check']
+                             'skip_dialog_check', 'login_close']
     },
     'windows': {  # required  when supporting windows game
         'top_hwnd_class': [re.compile('CAgreementDlg'), re.compile('CLoginDlg_P_'),
